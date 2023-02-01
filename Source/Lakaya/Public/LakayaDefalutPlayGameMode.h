5be75465b9e47484cca8783e75f26d4872d1c0ea
zzx0147 <zzx0147@naver.com>
2023-02-01 22:06:50 +0900

    로비용 게임모드, 플레이용 게임모드 두개를 만들고 기존의 게임모드를 정리했습니다

---
+++ Source/Lakaya/Public/LakayaDefalutPlayGameMode.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameMode.h"
+#include "LakayaDefalutPlayGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class LAKAYA_API ALakayaDefalutPlayGameMode : public AGameMode
+{
+	GENERATED_BODY()
+	
+};
---
+++ Source/Lakaya/Public/LakayaMainLobbyGameMode.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameMode.h"
+#include "LakayaMainLobbyGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class LAKAYA_API ALakayaMainLobbyGameMode : public AGameMode
+{
+	GENERATED_BODY()
+	
+};
