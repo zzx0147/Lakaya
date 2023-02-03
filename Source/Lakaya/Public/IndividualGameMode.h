083523c00c6b69baa0d9b737626c363403bc6525
KwonDoJun <bluetooth3333@naver.com>
2023-02-03 14:15:53 +0900

    20230203_1415
    IndividualGameModeCPP Add.

---
+++ Source/Lakaya/Public/IndividualGameMode.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "LakayaDefalutPlayGameMode.h"
+#include "IndividualGameMode.generated.h"
+
+UCLASS()
+class LAKAYA_API AIndividualGameMode : public ALakayaDefalutPlayGameMode
+{
+	GENERATED_BODY()
+	
+};
