bde03abe1b1b20ceecf017f93e0d098881af7b36
yongwoo117 <ywhss117@daum.net>
2023-02-10 17:13:24 +0900

    Add PlayerState, PlayerController
    InputAction의 이름도 변경했습니다.

---
+++ Source/Lakaya/Public/PointCollectingPlayerState.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerState.h"
+#include "PointCollectingPlayerState.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class LAKAYA_API APointCollectingPlayerState : public APlayerState
+{
+	GENERATED_BODY()
+	
+};
