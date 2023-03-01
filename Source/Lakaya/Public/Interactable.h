b9970682e43e50f7ed51b753ddf1a8e4ea7620a9
yongwoo117 <ywhss117@daum.net>
2023-03-01 18:07:27 +0900

    Add some comments

--- Source/Lakaya/Public/Interactable.h
+++ Source/Lakaya/Public/Interactable.h
@@ -22,9 +22,19 @@ class LAKAYA_API IInteractable : public ILockstep
 
 	// Add interface functions to this class. This is the class that will be inherited to implement this interface.
 public:
+	/**
+	 * @brief 인터렉션 시작을 서버에 요청합니다. 직접 호출하지 않고, Execute(IInteractable::Execute_InteractionStart)와 같이 호출합니다.
+	 * @param Time 클라이언트에서 인터렉션을 시작한 시간입니다.
+	 * @param CallerId 플레이어의 UniqueID입니다.
+	 */
 	UFUNCTION(Server, Reliable)
 	virtual void InteractionStart(const float& Time, const FUniqueNetIdRepl& CallerId);
 
+	/**
+	 * @brief 인터렉션 중단을 서버에 요청합니다. 직접 호출하지 않고, Execute(IInteractable::Execute_InteractionStop)와 같이 호출합니다.
+	 * @param Time 클라이언트에서 인터렉션을 중단한 시간입니다.
+	 * @param CallerId 플레이어의 UniqueID입니다.
+	 */
 	UFUNCTION(Server, Reliable)
 	virtual void InteractionStop(const float& Time, const FUniqueNetIdRepl& CallerId);
 };
--- Source/Lakaya/Public/Lockstep.h
+++ Source/Lakaya/Public/Lockstep.h
@@ -21,8 +21,20 @@ class LAKAYA_API ILockstep
 	GENERATED_BODY()
 
 public:
+	/**
+	 * @brief 결정론적 락스텝 구현을 위해 서버와 클라이언트가 기다릴 시간입니다.
+	 */
 	inline static constexpr float LockstepDelay = 0.1f;
-	
+
+	/**
+	 * @brief 인터페이스에 선언되어 직접 호출할 수 없는 락스텝 함수의 Execute_ 함수를 좀 더 편리하게 호출합니다.
+	 * @param ExecuteFunction 실행하려는 'Execute_'함수
+	 */
 	void Execute(void (*ExecuteFunction)(UObject*, const float&));
+	/**
+	 * @brief 인터페이스에 선언되어 직접 호출할 수 없는 락스텝 함수의 Execute_ 함수를 좀 더 편리하게 호출합니다. 현재 이 함수는 IInteraction을 위해서만 사용됩니다.
+	 * @param ExecuteFunction 실행하려는 'Execute_'함수
+	 * @param Caller 인터렉션을 요청하는 플레이어의 폰
+	 */
 	void Execute(void (*ExecuteFunction)(UObject*, const float&, const FUniqueNetIdRepl&), const APawn* Caller);
 };
