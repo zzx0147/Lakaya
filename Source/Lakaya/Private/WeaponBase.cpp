2f9b2bd3da77d7129f403f03ece7654efb992dfc
yongwoo117 <ywhss117@daum.net>
2023-03-05 23:26:34 +0900

    Add event-based structure
    우선순위 큐를 이용해 이벤트를 저장하고, 이벤트의 실행시간이 도래하면 호출하는 기능을 추가했습니다.

--- Source/Lakaya/Private/WeaponBase.cpp
+++ Source/Lakaya/Private/WeaponBase.cpp
@@ -2,3 +2,28 @@
 
 
 #include "WeaponBase.h"
+
+void UWeaponBase::ApplyEvent(const uint8& EventNumber, const float& RequestTime)
+{
+	auto CurrentTime = GetServerTime();
+	auto ExecutionTime = RequestTime + LockstepDelay;
+
+	if (ExecutionTime < CurrentTime)
+	{
+		ExecuteLateEvent(EventNumber, RequestTime, CurrentTime);
+		return;
+	}
+
+	PriorityQueue.push({ExecutionTime, EventNumber});
+	GetWorld()->GetGameState()->GetWorldTimerManager().SetTimer(EventTimer, this, &UWeaponBase::EventTimerCallback,
+	                                                            ExecutionTime - CurrentTime);
+}
+
+void UWeaponBase::EventTimerCallback()
+{
+	ExecuteEvent(PriorityQueue.top().EventNumber);
+	PriorityQueue.pop();
+	if (PriorityQueue.empty()) return;
+	GetWorld()->GetGameState()->GetWorldTimerManager().SetTimer(EventTimer, this, &UWeaponBase::EventTimerCallback,
+	                                                            PriorityQueue.top().ExecutionTime - GetServerTime());
+}
--- Source/Lakaya/Public/WeaponBase.h
+++ Source/Lakaya/Public/WeaponBase.h
@@ -4,7 +4,21 @@
 
 #include "CoreMinimal.h"
 #include "UObject/Object.h"
+#include "queue"
+#include "GameFramework/GameStateBase.h"
 #include "WeaponBase.generated.h"
+
+USTRUCT()
+struct FEventInfoStruct
+{
+	GENERATED_BODY()
+
+public:
+	inline bool operator<(const FEventInfoStruct& Other) const { return ExecutionTime < Other.ExecutionTime; }
+	inline bool operator>(const FEventInfoStruct& Other) const { return ExecutionTime > Other.ExecutionTime; }
+	float ExecutionTime;
+	uint8 EventNumber;
+};
 
 /**
  * 
@@ -20,7 +34,40 @@ public:
 
 protected:
 	virtual void SetupData_Implementation(const FName& RowName) { return; }
+	inline float GetServerTime() { return GetWorld()->GetGameState()->GetServerWorldTimeSeconds(); }
 
+	/**
+	 * @brief 락스텝 이벤트를 등록합니다.
+	 * @param EventNumber 이벤트를 구분하는 값입니다.
+	 * @param RequestTime 최초 이벤트가 요청된 시간입니다.
+	 */
+	void ApplyEvent(const uint8& EventNumber, const float& RequestTime);
+
+	/**
+	 * @brief 이벤트가 실행될 시간이 도래하면 호출됩니다.
+	 * @param EventNumber 이벤트를 구분하는 값입니다.
+	 */
+	virtual void ExecuteEvent(const uint8& EventNumber) { return; }
+
+	/**
+	 * @brief 클라이언트로부터 요청된 이벤트가 너무 늦게 도착한 경우 호출됩니다.
+	 * @param EventNumber 이벤트를 구분하는 값입니다.
+	 * @param RequestTime 최초 이벤트가 요청된 시간입니다.
+	 * @param CurrentTime 현재 시간입니다.
+	 */
+	virtual void ExecuteLateEvent(const uint8& EventNumber, const float& RequestTime, const float& CurrentTime)
+	{
+		return;
+	}
+
+private:
+	void EventTimerCallback();
+
+protected:
 	UPROPERTY(Config)
 	float LockstepDelay = 0.1f;
+
+private:
+	std::priority_queue<FEventInfoStruct, std::vector<FEventInfoStruct>, std::greater<FEventInfoStruct>> PriorityQueue;
+	FTimerHandle EventTimer;
 };
