d6af0fae1b737d3d87c7b873c4b43e3a8fd2ec6a
NaHoGyeom <zzx0147@naver.com>
2023-03-16 15:04:28 +0900

    add function select character and select weapon(using Drag and Drop)
    준비 UI를 만들고 캐릭터 선택 기능과 무기 선택 기능을 추가하였습니다. 무기 선택은 드래그앤 드랍으로 선택하는 기능이 들어있습니다

--- Source/Lakaya/Public/GameLobbyWidget.h
+++ Source/Lakaya/Public/GameLobbyWidget.h
@@ -10,6 +10,12 @@
 /**
  * 
  */
+
+//선택한 캐릭터가 변경되었을 때 작동하는 델리게이트, 해당 캐릭터의 이름을 넘겨줍니다
+DECLARE_MULTICAST_DELEGATE_OneParam(OnChangeSelectedCharacterDelegate,FName)
+//선택한 무기가 변경되었을 때 작동하는 델리게이트, 변경된 무기의 슬롯 넘버와 변경된 무기 이름을 반환합니다
+DECLARE_MULTICAST_DELEGATE_TwoParams(OnChangeSelectedWeaponDelegate,int32,FName)
+
 UCLASS()
 class LAKAYA_API UGameLobbyWidget : public UUserWidget
 {
@@ -20,6 +26,8 @@ public:
 
 protected:
 	virtual void NativeConstruct() override;
+	
+	virtual void NativeTick(const FGeometry& MyGeometry, float InDeltaTime) override;
 
 public:
 
@@ -28,28 +36,60 @@ protected:
 	void ChangeSelectedCharacterImage(FString ImagePath);
 
 	UFUNCTION()
-	void OnClickedCitizenButton();
+	void OnClickedCharacter1Button();
+	UFUNCTION()
+	void OnClickedCharacter2Button();
+	UFUNCTION()
+	void OnClickedCharacter3Button();
+
+	void SelectCharacter(int32 CharacterNum);
 
 	UFUNCTION()
-	void OnClickedGovernmentManButton();
+	void OnPressedDraggableWeapon1Button();
+	UFUNCTION()
+	void OnPressedDraggableWeapon2Button();
+	UFUNCTION()
+	void OnPressedDraggableWeapon3Button();
+	UFUNCTION()
+	void OnPressedDraggableWeapon4Button();
+
+	void OnPressDraggableWeaponButton(int WeaponNum);
 
 	UFUNCTION()
-	void OnClickedGangsterButton();
+	void OnReleasedDraggableWeapon1Button();
+	UFUNCTION()
+	void OnReleasedDraggableWeapon2Button();
+	UFUNCTION()
+	void OnReleasedDraggableWeapon3Button();
+	UFUNCTION()
+	void OnReleasedDraggableWeapon4Button();
 
+	void OnReleasedDraggableWeaponButton(int WeaponNum);
 
+	UFUNCTION(BlueprintCallable)
+	int32 GetWeaponSlotAtLocation(FVector2D AbsoluteLocation);
 
+public:
+	OnChangeSelectedCharacterDelegate OnChangeSelectedCharacter;
+	OnChangeSelectedWeaponDelegate OnChangeSelectedWeapon;
 protected:
-
 	UPROPERTY(VisibleAnywhere)
 	UImage* SelectedCharacterImage;
 
-	UPROPERTY(VisibleAnywhere)
-	UButton* CitizenButton;
+	UButton* PrevCharacterButton;
 
-	UPROPERTY(VisibleAnywhere)
-	UButton* GovernmentManButton;
+	UImage* DragImage;
 
-	UPROPERTY(VisibleAnywhere)
-	UButton* GangsterButton;
+	bool isDragOn = false;
 
+	FVector2D CurrentMousePosisionWhileDrag;
+
+	TArray<UButton*> CharacterButtonArray;
+	TArray<UMaterialInterface*> CharacterRenderTargetMaterialArray;
+	TArray<FName> CharacterNameArray;
+	TArray<FName> WeaponNameArray;
+	TArray<UImage*> WeaponSlotArray;
+	TArray<UButton*> DraggableWeaponButtonArray;
+	TArray<UTexture2D*> WeaponTextureArray;
+	TArray<int32> SelectedWeaponArray;
 };
