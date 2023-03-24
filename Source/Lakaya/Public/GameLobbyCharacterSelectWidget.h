3c944d813df718c4cd72e9590488b659e5f79954
NaHoGyeom <zzx0147@naver.com>
2023-03-24 22:24:34 +0900

    add comment and Refactor Lobby Widget

---
+++ Source/Lakaya/Public/GameLobbyCharacterSelectWidget.h
@@ -0,0 +1,46 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameLobbyWeaponSelectWidget.h"
+#include "GameLobbyCharacterSelectWidget.generated.h"
+
+
+//선택한 캐릭터가 변경되었을 때 작동하는 델리게이트, 해당 캐릭터의 이름을 넘겨줍니다
+DECLARE_MULTICAST_DELEGATE_OneParam(OnChangeSelectedCharacterDelegate, FName)
+
+UCLASS()
+class LAKAYA_API UGameLobbyCharacterSelectWidget : public UGameLobbyWeaponSelectWidget
+{
+	GENERATED_BODY()
+
+public:
+	UGameLobbyCharacterSelectWidget(const FObjectInitializer& ObjectInitializer);
+
+protected:
+	virtual void NativeConstruct() override;
+	virtual void NativeTick(const FGeometry& MyGeometry, float InDeltaTime) override;
+
+	void ChangeSelectedCharacterImage(FString ImagePath);
+
+	UFUNCTION()
+	void OnClickedCharacter1Button();
+	UFUNCTION()
+	void OnClickedCharacter2Button();
+	UFUNCTION()
+	void OnClickedCharacter3Button();
+
+	void SelectCharacter(int32 CharacterNum);
+
+public:
+	OnChangeSelectedCharacterDelegate OnChangeSelectedCharacter;
+
+private:
+	TArray<UButton*> CharacterButtonArray;
+	TArray<UMaterialInterface*> CharacterRenderTargetMaterialArray;
+	TArray<FName> CharacterNameArray;
+
+	UPROPERTY(VisibleAnywhere)
+	UImage* SelectedCharacterImage;
+
+	UButton* PrevCharacterButton;
+};
