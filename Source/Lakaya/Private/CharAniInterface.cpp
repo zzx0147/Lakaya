a0420c8abc820907bcc72302e2aacc127f5c8c60
BinHenz <dstjer@naver.com>
2023-03-24 22:27:02 +0900

    Character Animation AnimCalss Create
    캐릭터 애님 인스턴스를 상속받는 클래스를 생성하였습니당
    사유는 애니메이션 재생할때를 C++에서 관리하기위해입니당

--- Source/Lakaya/Private/CharAniInterface.cpp
+++
@@ -1,7 +0,0 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-
-#include "CharAniInterface.h"
-
-#include "GameFramework/GameStateBase.h"
-#include "GameFramework/PlayerState.h"
