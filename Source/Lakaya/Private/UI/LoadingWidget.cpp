a92220d620102303cab1042f3b654b6e753ce87e
yongwoo117 <ywhss117@daum.net>
2023-04-14 15:47:20 +0900

    Update LoadingWidget.cpp
    누락된 커밋

--- Source/Lakaya/Private/UI/LoadingWidget.cpp
+++ Source/Lakaya/Private/UI/LoadingWidget.cpp
@@ -1,67 +1,45 @@
 #include "UI/LoadingWidget.h"
 
-#include "PlayerController/MenuCallingPlayerController.h"
-#include "GameMode/IndividualGameMode.h"
-#include "GameMode/IndividualGameState.h"
-#include "Net/UnrealNetwork.h"
+#include "GameMode/OccupationGameState.h"
+
 
 void ULoadingWidget::NativeConstruct()
 {
-    Super::NativeConstruct();
+	Super::NativeConstruct();
 
-    AOccupationGameState* OccupationGameState = Cast<AOccupationGameState>(GetWorld()->GetGameState());
-    if (OccupationGameState == nullptr)
-    {
-        UE_LOG(LogTemp, Warning, TEXT("LoadingWidget_GameMode is null."));
-        return;
-    }
+	AOccupationGameState* OccupationGameState = GetWorld()->GetGameState<AOccupationGameState>();
+	if (OccupationGameState == nullptr)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("LoadingWidget_GameMode is null."));
+		return;
+	}
 
-    OnChangeJoinedPlayers(OccupationGameState->GetNumPlayers(), OccupationGameState->GetMaxPlayers());
+	OnChangeJoinedPlayers(OccupationGameState->GetNumPlayers(), OccupationGameState->GetMaxPlayers());
 
-    // 바인딩
-    LoadingWidgetText = Cast<UTextBlock>(GetWidgetFromName(TEXT("LoadingWidgetText")));
-    if (LoadingWidgetText == nullptr)
-    {
-        UE_LOG(LogTemp, Warning, TEXT("LoadingWidget_JoinedPlayerText is null."));
-        return;
-    }
+	// 바인딩
+	LoadingWidgetText = Cast<UTextBlock>(GetWidgetFromName(TEXT("LoadingWidgetText")));
+	if (LoadingWidgetText == nullptr)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("LoadingWidget_JoinedPlayerText is null."));
+		return;
+	}
 
-    OccupationGameState->OnOccupationChangeJoinedPlayers.AddUObject(this, &ULoadingWidget::OnChangeJoinedPlayers);
-    OccupationGameState->OnOccupationChangeMaxPlayers.AddUObject(this, &ULoadingWidget::OnChangeMaxPlayers);
-    OccupationGameState->OnOccupationChangeGameState.AddUObject(this, &ULoadingWidget::ReMoveLoadingWidget);
-}
-
-void ULoadingWidget::NativeTick(const FGeometry& MyGeometry, float InDeltaTime)
-{
-    Super::NativeTick(MyGeometry, InDeltaTime);
+	OccupationGameState->OnOccupationChangeJoinedPlayers.AddUObject(this, &ULoadingWidget::OnChangeJoinedPlayers);
+	OccupationGameState->OnOccupationChangeMaxPlayers.AddUObject(this, &ULoadingWidget::OnChangeMaxPlayers);
 }
 
 void ULoadingWidget::OnChangeJoinedPlayers(uint8 JoinedPlayers, uint8 MaxPlayer)
 {
-    if (JoinedPlayers == MaxPlayer)
-    {
-        LoadingWidgetText->SetText(FText::FromString(FString::Printf(TEXT("곧 게임을 시작합니다."))));
-        return;
-    }
-    
-    LoadingWidgetText->SetText(FText::FromString(FString::Printf(TEXT("(%d / %d)"), JoinedPlayers, MaxPlayer)));
-}
+	if (JoinedPlayers == MaxPlayer)
+	{
+		LoadingWidgetText->SetText(FText::FromString(FString::Printf(TEXT("곧 게임을 시작합니다."))));
+		return;
+	}
 
-void ULoadingWidget::ReMoveLoadingWidget(EOccupationGameState ChangeGamState)
-{
-    if (ChangeGamState == EOccupationGameState::Progress)
-    {
-        this->RemoveFromParent();
-    }
+	LoadingWidgetText->SetText(FText::FromString(FString::Printf(TEXT("(%d / %d)"), JoinedPlayers, MaxPlayer)));
 }
 
 void ULoadingWidget::OnChangeMaxPlayers(uint8 Number, uint8 MaxPlayers)
 {
-    // if (Number == MaxPlayers)
-    // {
-    //     LoadingWidgetText->SetText(FText::FromString(FString::Printf(TEXT("곧 게임을 시작합니다."))));
-    //     return;
-    // }
-    
-    LoadingWidgetText->SetText(FText::FromString(FString::Printf(TEXT("(%d / %d)"), Number, MaxPlayers)));
+	LoadingWidgetText->SetText(FText::FromString(FString::Printf(TEXT("(%d / %d)"), Number, MaxPlayers)));
 }
