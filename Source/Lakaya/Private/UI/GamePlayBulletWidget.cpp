57ad4a5b8d574f54f4a14f658e64c6c99f9c9a4e
yongwoo117 <ywhss117@daum.net>
2023-04-15 22:02:37 +0900

    Refactor BulletWidget

--- Source/Lakaya/Private/UI/GamePlayBulletWidget.cpp
+++ Source/Lakaya/Private/UI/GamePlayBulletWidget.cpp
@@ -2,9 +2,35 @@
 
 #include "UI/GamePlayBulletWidget.h"
 
+#include "Character/BulletComponent.h"
+#include "Character/LakayaBaseCharacter.h"
 #include "Components/TextBlock.h"
-#include "Weapon/GunComponent.h"
 
+
+void UGamePlayBulletWidget::BindCharacter(ACharacter* const& Character)
+{
+	Super::BindCharacter(Character);
+	if (const auto CastedCharacter = Cast<ALakayaBaseCharacter>(Character))
+		if (const auto BulletComponent = CastedCharacter->GetResourceComponent<UBulletComponent>())
+		{
+			OnChangeRemainBullets(BulletComponent->GetBullets());
+			OnChangeMagazineCapacity(BulletComponent->GetMaxBullets());
+
+			BulletComponent->OnBulletsChanged.AddUObject(this, &UGamePlayBulletWidget::OnChangeRemainBullets);
+			BulletComponent->OnMaxBulletsChanged.AddUObject(this, &UGamePlayBulletWidget::OnChangeMagazineCapacity);
+		}
+}
+
+bool UGamePlayBulletWidget::UnbindCharacter(ACharacter* const& Character)
+{
+	if (const auto CastedCharacter = Cast<ALakayaBaseCharacter>(Character))
+		if (const auto BulletComponent = CastedCharacter->GetResourceComponent<UBulletComponent>())
+		{
+			BulletComponent->OnBulletsChanged.RemoveAll(this);
+			BulletComponent->OnMaxBulletsChanged.RemoveAll(this);
+		}
+	return Super::UnbindCharacter(Character);
+}
 
 void UGamePlayBulletWidget::NativeConstruct()
 {
@@ -22,37 +48,13 @@ void UGamePlayBulletWidget::NativeConstruct()
 #pragma endregion
 }
 
-void UGamePlayBulletWidget::BindWeapon(UGunComponent* const& GunComponent)
-{
-	if (GunComponent)
-	{
-		GunComponent->OnCurrentBulletChanged.AddUObject(this, &UGamePlayBulletWidget::OnChangeRemainBullets);
-		GunComponent->OnMaximumBulletChanged.AddUObject(this, &UGamePlayBulletWidget::OnChangeMagazineCapacity);
-
-		OnChangeRemainBullets(GunComponent->GetRemainBullets());
-		OnChangeMagazineCapacity(GunComponent->GetMagazineCapacity());
-	}
-}
-
-void UGamePlayBulletWidget::UnBindWeapon(UGunComponent* const& GunComponent)
-{
-	if (GunComponent)
-	{
-		GunComponent->OnCurrentBulletChanged.RemoveAll(this);
-		GunComponent->OnMaximumBulletChanged.RemoveAll(this);
-
-		OnChangeRemainBullets(0);
-		OnChangeMagazineCapacity(0);
-	}
-}
-
-void UGamePlayBulletWidget::OnChangeRemainBullets(const uint16& NewRemainBullets)
+void UGamePlayBulletWidget::OnChangeRemainBullets(const uint16& NewRemainBullets) const
 {
 	//업데이트된 총알 갯수를 저장하고 텍스트로 표기
 	RemainBulletsText->SetText(FText::AsNumber(NewRemainBullets));
 }
 
-void UGamePlayBulletWidget::OnChangeMagazineCapacity(const uint16& NewMagazineCapacity)
+void UGamePlayBulletWidget::OnChangeMagazineCapacity(const uint16& NewMagazineCapacity) const
 {
 	//업데이트된 최대 총알 갯수를 저장하고 텍스트로 표기(앞에 /를 붙여서 표기)
 	MagazineCapacityText->SetText(FText::FromString(FString::Printf(TEXT("/%d"), NewMagazineCapacity)));
