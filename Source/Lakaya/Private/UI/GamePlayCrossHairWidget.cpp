ed550733edba64c12550366ab2b89673f2356944
yongwoo117 <ywhss117@daum.net>
2023-04-15 20:25:45 +0900

    Refactor CrosshairWidget

--- Source/Lakaya/Private/UI/GamePlayCrossHairWidget.cpp
+++ Source/Lakaya/Private/UI/GamePlayCrossHairWidget.cpp
@@ -4,7 +4,6 @@
 
 #include "UI/GamePlayCrosshairWidget.h"
 #include "Components/Image.h"
-#include "GameMode/IndividualGameState.h"
 
 void UGamePlayCrosshairWidget::NativeConstruct()
 {
@@ -17,29 +16,4 @@ void UGamePlayCrosshairWidget::NativeConstruct()
 	check(CrosshairImage != nullptr);
 
 #pragma endregion
-
-	AOccupationGameState* OccupationGameState = Cast<AOccupationGameState>(GetWorld()->GetGameState());
-	if (OccupationGameState == nullptr)
-	{
-		UE_LOG(LogTemp, Warning, TEXT("GamePlayCrossHairWidget_GameMode is null."));
-		return;
-	}
-
-	OccupationGameState->OnOccupationChangeGameState.AddUObject(this, &UGamePlayCrosshairWidget::SetGamePlayCrosshairWidget);
-
-	SetVisibility(ESlateVisibility::Hidden);
-}
-
-void UGamePlayCrosshairWidget::NativeTick(const FGeometry& MyGeometry, float InDeltaTime)
-{
-	Super::NativeTick(MyGeometry, InDeltaTime);
-}
-
-void UGamePlayCrosshairWidget::SetGamePlayCrosshairWidget(EOccupationGameState ChangeGameState)
-{
-	if (ChangeGameState == EOccupationGameState::Progress)
-	{
-		SetVisibility(ESlateVisibility::Visible);
-		return;
-	}
 }
