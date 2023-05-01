8f7c2b1b668ea4eacfdb557e573886a923ac8cf2
yongwoo117 <ywhss117@daum.net>
2023-05-01 21:53:20 +0900

    Update OccupationScoreBoard.cpp
    RegisterPlayer가 호출되었을 때, 플레이어가 아직 팀이 설정되지 않았더라도 문제없이 동작할 수 있도록 로직을 추가했습니다.

--- Source/Lakaya/Private/UI/OccupationScoreBoard.cpp
+++ Source/Lakaya/Private/UI/OccupationScoreBoard.cpp
@@ -20,28 +20,58 @@ void UOccupationScoreBoard::RegisterPlayer(APlayerState* PlayerState)
 	const auto Element = CreateWidget<UScoreBoardElement>(this, ElementClass);
 	if (!LakayaState || !Element) return;
 
-	//TODO: 클라이언트에서는 SelectCharacter 매치스테이트에서 아직 팀이 리플리케이트 되지 않았을 수 있으므로 이에 대한 예외처리가 필요합니다.
-	switch (const auto& Team = LakayaState->GetTeam())
+	switch (LakayaState->GetTeam())
 	{
 	case EPlayerTeam::A:
-		if (ATeamBox.IsValid()) ATeamBox->AddChildToVerticalBox(Element);
+		if (ATeamBox.IsValid())
+		{
+			ATeamBox->AddChildToVerticalBox(Element);
+			Element->SetVisibility(ESlateVisibility::SelfHitTestInvisible);
+		}
 		break;
 	case EPlayerTeam::B:
-		if (BTeamBox.IsValid()) BTeamBox->AddChildToVerticalBox(Element);
+		if (BTeamBox.IsValid())
+		{
+			BTeamBox->AddChildToVerticalBox(Element);
+			Element->SetVisibility(ESlateVisibility::SelfHitTestInvisible);
+		}
+		break;
+	case EPlayerTeam::None:
+		Element->AddToViewport();
+		Element->SetVisibility(ESlateVisibility::Collapsed);
 		break;
 	default:
+		UE_LOG(LogScript, Error, TEXT("Invalid team value on UOccupationScoreBoard::RegisterPlayer()!"));
 		Element->RemoveFromRoot();
 		return;
 	}
 
-	// 엘리먼트 바인딩
+	// 엘리먼트 데이터 바인딩
+	LakayaState->OnTeamChanged.AddLambda([this, Element](const EPlayerTeam& ArgTeam)
+	{
+		// A팀으로 변경되면 B팀의 버티컬 박스에서 엘리먼트를 제거하고 A팀 버티컬 박스에 엘리먼트를 추가합니다.
+		if (ArgTeam == EPlayerTeam::A)
+		{
+			if (ATeamBox.IsValid() && !ATeamBox->HasChild(Element)) ATeamBox->AddChildToVerticalBox(Element);
+			if (BTeamBox.IsValid()) BTeamBox->RemoveChild(Element);
+			Element->SetVisibility(ESlateVisibility::SelfHitTestInvisible);
+		}
+		// B팀으로 변경되면 A팀의 버티컬 박스에서 엘리먼트를 제거하고 B팀 버티컬 박스에 엘리먼트를 추가합니다.
+		else if (ArgTeam == EPlayerTeam::B)
+		{
+			if (ATeamBox.IsValid()) ATeamBox->RemoveChild(Element);
+			if (BTeamBox.IsValid() && !BTeamBox->HasChild(Element)) BTeamBox->AddChildToVerticalBox(Element);
+			Element->SetVisibility(ESlateVisibility::SelfHitTestInvisible);
+		}
+		else UE_LOG(LogScript, Error, TEXT("Invalid team value on OnTeamChanged lambda handler!"));
+	});
 	LakayaState->OnDeathCountChanged.AddUObject(Element, &UScoreBoardElement::SetDeathCount);
 	LakayaState->OnKillCountChanged.AddUObject(Element, &UScoreBoardElement::SetKillCount);
 
+	//TODO: 플레이어 이름이 잘 표시되는지 검증 필요. 리플리케이트가 늦어서 플레이어 이름이 제대로 표시되지 않는다면 플레이어 이름 변경 이벤트 선언 필요
 	Element->SetPlayerName(FText::FromString(LakayaState->GetPlayerName()));
 	Element->SetDeathCount(LakayaState->GetDeathCount());
 	Element->SetKillCount(LakayaState->GetKillCount());
-	Element->SetVisibility(ESlateVisibility::SelfHitTestInvisible);
 }
 
 void UOccupationScoreBoard::NativeConstruct()
