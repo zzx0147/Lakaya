5d4eef733b036acf40e9bcff607de1aa7c9efb9d
yongwoo117 <ywhss117@daum.net>
2023-05-06 00:58:03 +0900

    Add kill streak buff level
    연속처치 버프 구현을 위한 코드를 추가했습니다.

--- Source/Lakaya/Private/Character/BuffPlayerState.cpp
+++ Source/Lakaya/Private/Character/BuffPlayerState.cpp
@@ -2,3 +2,63 @@
 
 
 #include "Character/BuffPlayerState.h"
+
+#include "Character/LakayaBaseCharacter.h"
+#include "Net/UnrealNetwork.h"
+
+void ABuffPlayerState::GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const
+{
+	Super::GetLifetimeReplicatedProps(OutLifetimeProps);
+	DOREPLIFETIME(ABuffPlayerState, KillStreakBuffLevel);
+}
+
+void ABuffPlayerState::IncreaseKillStreak()
+{
+	Super::IncreaseKillStreak();
+
+	// 캐릭터로부터 연속처치시 적용될 버프 정보를 가져와서 버프를 적용합니다.
+	if (const auto Character = GetPawn<ALakayaBaseCharacter>())
+		if (auto& Buffs = Character->GetKillStreakBuffs(); Buffs.IsValidIndex(KillStreakBuffLevel))
+		{
+			ApplyBuff(Buffs[KillStreakBuffLevel]);
+			SetKillStreakBuffLevel(KillStreakBuffLevel + 1);
+		}
+}
+
+void ABuffPlayerState::OnPawnSetCallback(APlayerState* Player, APawn* NewPawn, APawn* OldPawn)
+{
+	Super::OnPawnSetCallback(Player, NewPawn, OldPawn);
+
+	if (const auto Character = Cast<ALakayaBaseCharacter>(OldPawn); Character && KillStreakBuffLevel != 0)
+	{
+		auto& Buffs = Character->GetKillStreakBuffs();
+
+		// 연속처치 시스템에 의해 적용되었던 버프들을 즉시 제거합니다.
+		for (int16 Index = KillStreakBuffLevel; Index >= 0; --Index)
+			if (Buffs.IsValidIndex(Index))
+				ExpireBuff(Buffs[Index]);
+
+		SetKillStreakBuffLevel(0);
+	}
+}
+
+void ABuffPlayerState::ApplyBuff(const FName& BuffName)
+{
+	//TODO: 데이터테이블에서 버프를 찾고 적용합니다.
+}
+
+void ABuffPlayerState::ExpireBuff(const FName& BuffName)
+{
+	//TODO: 버프 목록에서 버프를 찾아 적용된 버프를 제거합니다.
+}
+
+void ABuffPlayerState::OnRep_KillStreakBuffLevel()
+{
+	OnKillStreakBuffLevelChanged.Broadcast(KillStreakBuffLevel);
+}
+
+void ABuffPlayerState::SetKillStreakBuffLevel(const uint8& NewLevel)
+{
+	KillStreakBuffLevel = NewLevel;
+	OnKillStreakBuffLevelChanged.Broadcast(KillStreakBuffLevel);
+}
--- Source/Lakaya/Public/Character/BuffPlayerState.h
+++ Source/Lakaya/Public/Character/BuffPlayerState.h
@@ -6,8 +6,45 @@
 #include "StatPlayerState.h"
 #include "BuffPlayerState.generated.h"
 
+DECLARE_EVENT_OneParam(ABuffPlayerState, FKillLevelSignature, const uint8&)
+
 UCLASS()
 class LAKAYA_API ABuffPlayerState : public AStatPlayerState
 {
 	GENERATED_BODY()
+
+public:
+	virtual void IncreaseKillStreak() override;
+
+protected:
+	virtual void OnPawnSetCallback(APlayerState* Player, APawn* NewPawn, APawn* OldPawn) override;
+
+public:
+	/**
+	 * @brief 플레이어에게 버프를 적용합니다.
+	 * @param BuffName 적용할 버프의 이름입니다. 이 값을 통해 데이터테이블에서 버프를 찾아 적용합니다.
+	 */
+	UFUNCTION(BlueprintCallable)
+	void ApplyBuff(const FName& BuffName);
+
+	/**
+	 * @brief 플레이어에게 적용된 버프를 즉시 해제합니다. 제거하려는 버프가 없는 경우 아무 일도 일어나지 않습니다.
+	 * @param BuffName 해제할 버프의 이름입니다.
+	 */
+	UFUNCTION(BlueprintCallable)
+	void ExpireBuff(const FName& BuffName);
+
+protected:
+	UFUNCTION()
+	virtual void OnRep_KillStreakBuffLevel();
+
+	void SetKillStreakBuffLevel(const uint8& NewLevel);
+
+public:
+	// 적용중인 연속처치 버프 레벨이 변경되면 호출됩니다.
+	FKillLevelSignature OnKillStreakBuffLevelChanged;
+
+private:
+	UPROPERTY(ReplicatedUsing=OnRep_KillStreakBuffLevel, Transient)
+	uint8 KillStreakBuffLevel;
 };
