26cd3540d4c1e57cac1d4d2621f36815838bfac7
yongwoo117 <ywhss117@daum.net>
2023-05-05 19:09:32 +0900

    Create BuffPlayerState class
    버프를 관리하게 될 버프 플레이어 스테이트 클래스를 생성했습니다. 현재는 아무런 구현도 없습니다.

---
+++ Source/Lakaya/Public/Character/BuffPlayerState.h
@@ -0,0 +1,13 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "StatPlayerState.h"
+#include "BuffPlayerState.generated.h"
+
+UCLASS()
+class LAKAYA_API ABuffPlayerState : public AStatPlayerState
+{
+	GENERATED_BODY()
+};
