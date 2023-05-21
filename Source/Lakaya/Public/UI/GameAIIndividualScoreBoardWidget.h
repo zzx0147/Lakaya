8f03ac23cc6c0ac0b58e535c0f109b5443c26bc9
BinHenz <dstjer@naver.com>
2023-05-22 00:07:29 +0900

    AI Individual ScoreBoard Widget Create
    AI 개인전 모드 점수판 구현을 위해
    클래스와 UMG 생성했습니다.

---
+++ Source/Lakaya/Public/UI/GameAIIndividualScoreBoardWidget.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "GameAIIndividualScoreBoardWidget.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class LAKAYA_API UGameAIIndividualScoreBoardWidget : public UUserWidget
+{
+	GENERATED_BODY()
+	
+};
