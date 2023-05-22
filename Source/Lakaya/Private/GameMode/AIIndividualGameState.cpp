f65a30c50cf91bae1ed4aa36abab2159b5ef4777
BinHenz <dstjer@naver.com>
2023-05-22 23:53:50 +0900

    AI Individual Mode ScoreBoard Widget Create
    AI 개인전 모드에 사용될 점수판 (등수)
    구현완료되었습니다.
    현재 이름은 각 컨트롤러 이름으로 출력되게 되어있습니다.

--- Source/Lakaya/Private/GameMode/AIIndividualGameState.cpp
+++ Source/Lakaya/Private/GameMode/AIIndividualGameState.cpp
@@ -3,3 +3,105 @@
 
 #include "GameMode/AIIndividualGameState.h"
 
+#include "Character/LakayaBasePlayerState.h"
+#include "Kismet/GameplayStatics.h"
+
+AAIIndividualGameState::AAIIndividualGameState()
+{
+	PrimaryActorTick.bCanEverTick = true;
+	
+	static ConstructorHelpers::FClassFinder<UGameAIIndividualScoreBoardWidget> AIIndividualScoreBoardFinder(
+	TEXT("/Game/Blueprints/UMG/WBP_GameAIIndividualScoreBoardWidget"));
+
+	if (AIIndividualScoreBoardFinder.Succeeded()) AIIndividualScoreBoardWidgettClass = AIIndividualScoreBoardFinder.Class;
+}
+
+void AAIIndividualGameState::BeginPlay()
+{
+	Super::BeginPlay();
+
+	if (const auto LocalController = GetWorld()->GetFirstPlayerController<APlayerController>())
+	{
+		AIIndividualScoreBoardWidget = CreateWidget<UGameAIIndividualScoreBoardWidget>(LocalController, AIIndividualScoreBoardWidgettClass);
+		
+		if (AIIndividualScoreBoardWidget.IsValid())
+		{
+			AIIndividualScoreBoardWidget->AddToViewport();
+			AIIndividualScoreBoardWidget->SetVisibility(ESlateVisibility::Visible);
+			
+			if (const auto Other = LocalController->GetPlayerState<ALakayaBasePlayerState>())
+			{
+				Player1Data.PlayerName = Other->GetDebugName(LocalController);
+				Player2Data.PlayerName = Other->GetDebugName(LocalController);
+				Player3Data.PlayerName = Other->GetDebugName(LocalController);
+				Player4Data.PlayerName = Other->GetDebugName(LocalController);
+				Player5Data.PlayerName = Other->GetDebugName(LocalController);
+				Player6Data.PlayerName = Other->GetDebugName(LocalController);
+			
+				Player1Data.KillCount = Other->GetKillCount();
+				Player2Data.KillCount = Other->GetKillCount();
+				Player3Data.KillCount = Other->GetKillCount();
+				Player4Data.KillCount = Other->GetKillCount();
+				Player5Data.KillCount = Other->GetKillCount();
+				Player6Data.KillCount = Other->GetKillCount();
+				
+				UE_LOG(LogTemp, Warning, TEXT("Set All PlayerData In AIIndividualScoreBoardWidget"));
+			}
+			
+			FPlayerDataArray.Add(Player1Data);
+			FPlayerDataArray.Add(Player2Data);
+			FPlayerDataArray.Add(Player3Data);
+			FPlayerDataArray.Add(Player4Data);
+			FPlayerDataArray.Add(Player5Data);
+			FPlayerDataArray.Add(Player6Data);
+			
+			SetScoreBoardPlayerName(FPlayerDataArray);
+		}
+	}
+}
+
+void AAIIndividualGameState::Tick(float DeltaSeconds)
+{
+	Super::Tick(DeltaSeconds);
+
+	if (AIIndividualScoreBoardWidget.IsValid())
+	{
+		FPlayerDataArray.Empty();
+
+		for (FConstControllerIterator It = GetWorld()->GetControllerIterator(); It; ++It)
+		{
+			AController* PlayerController = It->Get();
+			if (PlayerController)
+			{
+				ALakayaBasePlayerState* PlayerStateObj = Cast<ALakayaBasePlayerState>(PlayerController->PlayerState);
+				if (PlayerStateObj)
+				{
+					// 점수판에 표시되는 이름 현제 컨트롤러 이름으로 표시중입니다.
+					PlayerData.PlayerName = PlayerStateObj->GetDebugName(PlayerController);
+					PlayerData.KillCount = PlayerStateObj->GetKillCount();
+					FPlayerDataArray.Add(PlayerData);
+					
+					// PlayerStateObj->OnKillCountChanged.AddLambda([this, PlayerStateObj]
+					// 	(const uint16& NewKillCount)
+					// {
+					// 	PlayerData.PlayerName = PlayerStateObj->GetCharacterName();
+					// 	PlayerData.KillCount = NewKillCount;
+					// 	FPlayerDataArray.Add(PlayerData);
+					//
+					// 	UE_LOG(LogTemp, Warning, TEXT("킬 적립!"));
+					// });
+				}
+			}
+		}
+		
+		SetScoreBoardPlayerName(FPlayerDataArray);
+	}
+}
+
+void AAIIndividualGameState::SetScoreBoardPlayerName(const TArray<FPlayerData>& PlayerDataArray)
+{
+	if (AIIndividualScoreBoardWidget.IsValid())
+	{
+		AIIndividualScoreBoardWidget->SetScoreBoardPlayerName(PlayerDataArray);
+	}
+}
--- Source/Lakaya/Private/UI/GameAIIndividualScoreBoardWidget.cpp
+++ Source/Lakaya/Private/UI/GameAIIndividualScoreBoardWidget.cpp
@@ -3,3 +3,63 @@
 
 #include "UI/GameAIIndividualScoreBoardWidget.h"
 
+#include "Components/Image.h"
+#include "Components/TextBlock.h"
+#include "GameFramework/Character.h"
+#include "GameFramework/PlayerState.h"
+
+UGameAIIndividualScoreBoardWidget::UGameAIIndividualScoreBoardWidget(const FObjectInitializer& ObjectInitializer) : Super(ObjectInitializer)
+{
+	// static const ConstructorHelpers::FClassFinder<UGameAIIndividualScoreBoardWidget> ElementFinder(
+	// TEXT("/Game/Blueprints/UMG/WBP_GameAIIndividualScoreBoardWidget"));
+}
+
+void UGameAIIndividualScoreBoardWidget::SetScoreBoardPlayerName(const TArray<FPlayerData>& PlayerDataArray)
+{
+	TArray<FPlayerData> SortedPlayerDataArray = PlayerDataArray;
+	SortedPlayerDataArray.Sort([](const FPlayerData& A, const FPlayerData& B) {
+		return A.KillCount > B.KillCount;
+	});
+	
+	for (int32 i = 0; i < SortedPlayerDataArray.Num(); i++)
+	{
+		const FPlayerData& PlayerData = SortedPlayerDataArray[i];
+
+		UTextBlock* NameTextBlock = nullptr;
+		UTextBlock* KillTextBlock = nullptr;
+		
+		switch (i)
+		{
+		case 0:
+			NameTextBlock = Score1stNameTextBlock;
+			KillTextBlock = Score1stKillCountTextBlock;
+			break;
+		case 1:
+			NameTextBlock = Score2ndNameTextBlock;
+			KillTextBlock = Score2ndKillCountTextBlock;
+			break;
+		case 2:
+			NameTextBlock = Score3rdNameTextBlock;
+			KillTextBlock = Score3rdKillCountTextBlock;
+			break;
+		case 3:
+			NameTextBlock = Score4thNameTextBlock;
+			KillTextBlock = Score4thKillCountTextBlock;
+			break;
+		case 4:
+			NameTextBlock = Score5thNameTextBlock;
+			KillTextBlock = Score5thKillCountTextBlock;
+			break;
+		case 5:
+			NameTextBlock = Score6thNameTextBlock;
+			KillTextBlock = Score6thKillCountTextBlock;
+			break;
+		}
+
+		if (NameTextBlock)
+		{
+			NameTextBlock->SetText(FText::FromString(PlayerData.PlayerName));
+			KillTextBlock->SetText(FText::AsNumber(PlayerData.KillCount));
+		}
+	}
+}
