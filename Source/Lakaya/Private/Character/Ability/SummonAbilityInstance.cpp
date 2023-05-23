e03f3279a645b9e820fdcd77f81b3308648e2856
yongwoo117 <ywhss117@daum.net>
2023-05-23 09:56:43 +0900

    Update ability summon structure
    어빌리티 인스턴스가 소환되고, 끝날 때 캐릭터가 알 수 있도록 이벤트를 추가하고, 호출구조를 변경했습니다.

--- Source/Lakaya/Private/Character/Ability/SummonAbilityInstance.cpp
+++ Source/Lakaya/Private/Character/Ability/SummonAbilityInstance.cpp
@@ -3,16 +3,31 @@
 
 #include "Character/Ability/SummonAbilityInstance.h"
 
+#include "Character/Ability/CoolTimedSummonAbility.h"
+#include "Net/UnrealNetwork.h"
+
 ASummonAbilityInstance::ASummonAbilityInstance(const FObjectInitializer& ObjectInitializer) : Super(ObjectInitializer)
 {
 	bReplicates = true;
 }
 
+void ASummonAbilityInstance::GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const
+{
+	Super::GetLifetimeReplicatedProps(OutLifetimeProps);
+	DOREPLIFETIME(ASummonAbilityInstance, OwningAbility)
+}
+
+
 void ASummonAbilityInstance::OnSummoned()
 {
 }
 
-void ASummonAbilityInstance::BroadcastOnAbilityEnded()
+void ASummonAbilityInstance::NotifyAbilitySummoned()
 {
-	OnAbilityEnded.Broadcast(this);
+	if (OwningAbility) OwningAbility->NotifyAbilityInstanceSummoned(this);
+}
+
+void ASummonAbilityInstance::NotifyAbilityEnded()
+{
+	if (OwningAbility) OwningAbility->NotifyAbilityInstanceEnded(this);
 }
