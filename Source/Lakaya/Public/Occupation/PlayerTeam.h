49fd92ee4a25ce78655b8e682d36bfc5b8ac294a
yongwoo117 <ywhss117@daum.net>
2023-05-26 15:21:34 +0900

    Add JudgeSameTeam
    플레이어 스테이트 없이도 같은 팀인지 판별할 수 있도록 로직을 분리했습니다.

--- Source/Lakaya/Public/Occupation/PlayerTeam.h
+++ Source/Lakaya/Public/Occupation/PlayerTeam.h
@@ -18,3 +18,9 @@ enum class EPlayerTeam : uint8
 	// 개인전
 	Individual UMETA(DisplayerName = "Individual")
 };
+
+inline bool JudgeSameTeam(const EPlayerTeam& First, const EPlayerTeam& Second)
+{
+	// 팀이 없거나 개인전 상태가 아니고, 두 팀의 값이 같다면 같은 팀으로 판정합니다.
+	return First != EPlayerTeam::None && First != EPlayerTeam::Individual && First == Second;
+}
