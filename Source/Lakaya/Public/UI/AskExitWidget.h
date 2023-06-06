0f5766a3cc5e982466d62d9c700d821c70e0a56b
KwonDoJun <bluetooth3333@naver.com>
2023-06-06 16:45:26 +0900

    GameExit Widget Completion.
    게임 종료 위젯을 작업했습니다.

---
+++ Source/Lakaya/Public/UI/AskExitWidget.h
@@ -0,0 +1,26 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "AskExitWidget.generated.h"
+
+UCLASS()
+class LAKAYA_API UAskExitWidget : public UUserWidget
+{
+	GENERATED_BODY()
+
+public:
+	virtual bool Initialize() override;
+
+	UFUNCTION(BlueprintCallable)
+	void QuitGame();
+
+	UFUNCTION(BlueprintCallable)
+	void BackLobby();
+protected:
+	UPROPERTY(BlueprintReadWrite, meta = (BindWidget))
+	TWeakObjectPtr<class UButton> AskExit_Yes_Button;
+
+	UPROPERTY(BlueprintReadWrite, meta = (BindWidget))
+	TWeakObjectPtr<UButton> AskExit_No_Button;
+};
