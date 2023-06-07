dcaabc19a1aba664d1cb5b2118ad30e0cfb55491
BinHenz <dstjer@naver.com>
2023-06-07 23:37:00 +0900

    Rena, Wazi AnimInstance Setting
    레나, 와지의 애님인스턴스를 새로 생성하여 ABP의 부모클래스를 바꿔주었습니다.
    CharAnimInstance 클래스의 자식으로
    RenaAnimInstance
    WaziAnimInstance
    이렇게 생성하여 바꿔주었습니다.

---
+++ Source/Lakaya/Public/AnimInstance/RenaAnimInstance.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AnimInstance/CharAnimInstance.h"
+#include "RenaAnimInstance.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class LAKAYA_API URenaAnimInstance : public UCharAnimInstance
+{
+	GENERATED_BODY()
+	
+};
---
+++ Source/Lakaya/Public/AnimInstance/WaziAnimInstance.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AnimInstance/CharAnimInstance.h"
+#include "WaziAnimInstance.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class LAKAYA_API UWaziAnimInstance : public UCharAnimInstance
+{
+	GENERATED_BODY()
+	
+};
