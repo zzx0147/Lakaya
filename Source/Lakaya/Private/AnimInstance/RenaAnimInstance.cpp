58c30cbdebd1a0931966daaa76f3180b3f4dcb61
BinHenz <dstjer@naver.com>
2023-06-08 00:15:54 +0900

    CharAnimInstance Update
    기존 캐릭터 애님인스턴스 클래스에 있는 스킬 애니메이션 배속함수와 트랜지션을 위한 변수를 각 레나, 와지 애님인스턴스 클래스로 옮겼습니다.
    이제 각 애님인스턴스 클래스에서 배속구현 수정해야합니다.

--- Source/Lakaya/Private/AnimInstance/RenaAnimInstance.cpp
+++ Source/Lakaya/Private/AnimInstance/RenaAnimInstance.cpp
@@ -3,3 +3,146 @@
 
 #include "AnimInstance/RenaAnimInstance.h"
 
+#include "Character/Ability/CoolTimedSummonAbility.h"
+#include "GameFramework/GameStateBase.h"
+
+URenaAnimInstance::URenaAnimInstance()
+{
+}
+
+void URenaAnimInstance::NativeBeginPlay()
+{
+	Super::NativeBeginPlay();
+	
+	if(const auto Character = Cast<AArmedCharacter>(TryGetPawnOwner()))
+	{
+		if (const auto Ability = Character->FindAbility<UCoolTimedSummonAbility>(WeaponAbility))
+		{
+			Ability->OnPerformTimeNotified.AddUObject(this, &URenaAnimInstance::OnWeaponAbilityPerformTimeNotified);
+		}
+		if (const auto Ability = Character->FindAbility<UCoolTimedSummonAbility>(Primary))
+		{
+			Ability->OnPerformTimeNotified.AddUObject(this, &URenaAnimInstance::OnPrimaryAbilityPerformTimeNotified);
+		}
+		if (const auto Ability = Character->FindAbility<UCoolTimedSummonAbility>(Secondary))
+		{
+			Ability->OnPerformTimeNotified.AddUObject(this, &URenaAnimInstance::OnSecondaryAbilityPerformTimeNotified);
+		}
+	}
+}
+
+void URenaAnimInstance::NativeUpdateAnimation(float DeltaSeconds)
+{
+	Super::NativeUpdateAnimation(DeltaSeconds);
+}
+
+void URenaAnimInstance::OnWeaponAbilityPerformTimeNotified(const float& Time)
+{
+	WeaponAbilityPerformTime = Time;
+	auto RemainTime = WeaponAbilityPerformTime - GetWorld()->GetGameState()->GetServerWorldTimeSeconds();
+	
+	// 아직 투사체 투척 시간이 도래하지 않은 경우 선딜레이 애니메이션 재생시간동안 배속을 걸어 재생하고, 이후에는 1배속으로 재생합니다.
+	if (RemainTime > 0.f)
+	{
+		WeaponSkillAnimSpeed = WeaponAbilityPerformDelayAnimDuration / RemainTime;
+		bIsWeaponSkill = true;
+
+		GetWorld()->GetTimerManager().SetTimer(WeaponAbilityAnimTimer, [this]
+		{
+			// 선딜레이 애니메이션이 종료되는 시점부터는 1배속으로 재생합니다.
+			WeaponSkillAnimSpeed = 1.f;
+			
+			// 전체 애니메이션이 종료되는 시간에 bIsWeaponSkill을 false로 바꿔줍니다.
+			GetWorld()->GetTimerManager().SetTimer(WeaponAbilityAnimTimer, [this]
+			{
+				bIsWeaponSkill = false;
+			}, WeaponAbilityLateAnimDuration, false);
+		}, RemainTime, false);
+	}
+	// 투사체 투척 시간을 이미 지나버렸지만 아직 전체 애니메이션 시간을 지나지는 않은 경우 애니메이션을 빨리 재생시킵니다.
+	else if (-RemainTime < WeaponAbilityLateAnimDuration)
+	{
+		// 이제 RemainTime은 전체 애니메이션이 종료되는 시점까지 남은 시간입니다.
+		RemainTime += WeaponAbilityLateAnimDuration;
+
+		// 전체 애니메이션 시간 / 남은시간을 통해 애니메이션 배속을 특정합니다.
+		WeaponSkillAnimSpeed = (WeaponAbilityPerformDelayAnimDuration + WeaponAbilityLateAnimDuration) / RemainTime;
+		bIsWeaponSkill = true;
+		GetWorld()->GetTimerManager().SetTimer(WeaponAbilityAnimTimer, [this] { bIsWeaponSkill = false; },
+		                                       RemainTime, false);
+	}
+}
+
+void URenaAnimInstance::OnPrimaryAbilityPerformTimeNotified(const float& Time)
+{
+	PrimaryAbilityPerformTime = Time;
+	auto RemainTime = PrimaryAbilityPerformTime - GetWorld()->GetGameState()->GetServerWorldTimeSeconds();
+
+	// 아직 투사체 투척 시간이 도래하지 않은 경우 선딜레이 애니메이션 재생시간동안 배속을 걸어 재생하고, 이후에는 1배속으로 재생합니다.
+	if (RemainTime > 0.f)
+	{
+		WeaponSkillAnimSpeed = PrimaryAbilityPerformDelayAnimDuration / RemainTime;
+		bIsPrimarySkill = true;
+
+		GetWorld()->GetTimerManager().SetTimer(PrimaryAbilityAnimTimer, [this]
+		{
+			// 선딜레이 애니메이션이 종료되는 시점부터는 1배속으로 재생합니다.
+			WeaponSkillAnimSpeed = 1.f;
+			
+			// 전체 애니메이션이 종료되는 시간에 bIsWeaponSkill을 false로 바꿔줍니다.
+			GetWorld()->GetTimerManager().SetTimer(PrimaryAbilityAnimTimer, [this]
+			{
+				bIsPrimarySkill = false;
+			}, PrimaryAbilityLateAnimDuration, false);
+		}, RemainTime, false);
+	}
+	// 투사체 투척 시간을 이미 지나버렸지만 아직 전체 애니메이션 시간을 지나지는 않은 경우 애니메이션을 빨리 재생시킵니다.
+	else if (-RemainTime < PrimaryAbilityLateAnimDuration)
+	{
+		// 이제 RemainTime은 전체 애니메이션이 종료되는 시점까지 남은 시간입니다.
+		RemainTime += PrimaryAbilityLateAnimDuration;
+
+		// 전체 애니메이션 시간 / 남은시간을 통해 애니메이션 배속을 특정합니다.
+		WeaponSkillAnimSpeed = (PrimaryAbilityPerformDelayAnimDuration + PrimaryAbilityLateAnimDuration) / RemainTime;
+		bIsPrimarySkill = true;
+		GetWorld()->GetTimerManager().SetTimer(PrimaryAbilityAnimTimer, [this] { bIsPrimarySkill = false; },
+											   RemainTime, false);
+	}
+}
+
+void URenaAnimInstance::OnSecondaryAbilityPerformTimeNotified(const float& Time)
+{
+	SecondaryAbilityPerformTime = Time;
+	auto RemainTime = SecondaryAbilityPerformTime - GetWorld()->GetGameState()->GetServerWorldTimeSeconds();
+
+	// 아직 투사체 투척 시간이 도래하지 않은 경우 선딜레이 애니메이션 재생시간동안 배속을 걸어 재생하고, 이후에는 1배속으로 재생합니다.
+	if (RemainTime > 0.f)
+	{
+		WeaponSkillAnimSpeed = SecondaryAbilityPerformDelayAnimDuration / RemainTime;
+		bIsSecondarySkill = true;
+
+		GetWorld()->GetTimerManager().SetTimer(SecondaryAbilityAnimTimer, [this]
+		{
+			// 선딜레이 애니메이션이 종료되는 시점부터는 1배속으로 재생합니다.
+			WeaponSkillAnimSpeed = 1.f;
+			
+			// 전체 애니메이션이 종료되는 시간에 bIsWeaponSkill을 false로 바꿔줍니다.
+			GetWorld()->GetTimerManager().SetTimer(SecondaryAbilityAnimTimer, [this]
+			{
+				bIsSecondarySkill = false;
+			}, SecondaryAbilityLateAnimDuration, false);
+		}, RemainTime, false);
+	}
+	// 투사체 투척 시간을 이미 지나버렸지만 아직 전체 애니메이션 시간을 지나지는 않은 경우 애니메이션을 빨리 재생시킵니다.
+	else if (-RemainTime < SecondaryAbilityLateAnimDuration)
+	{
+		// 이제 RemainTime은 전체 애니메이션이 종료되는 시점까지 남은 시간입니다.
+		RemainTime += SecondaryAbilityLateAnimDuration;
+
+		// 전체 애니메이션 시간 / 남은시간을 통해 애니메이션 배속을 특정합니다.
+		WeaponSkillAnimSpeed = (SecondaryAbilityPerformDelayAnimDuration + SecondaryAbilityLateAnimDuration) / RemainTime;
+		bIsSecondarySkill = true;
+		GetWorld()->GetTimerManager().SetTimer(SecondaryAbilityAnimTimer, [this] { bIsSecondarySkill = false; },
+											   RemainTime, false);
+	}
+}
--- Source/Lakaya/Public/AnimInstance/CharAnimInstance.h
+++ Source/Lakaya/Public/AnimInstance/CharAnimInstance.h
@@ -34,40 +34,6 @@ public:
 	UPROPERTY(EditAnywhere, BlueprintReadWrite)
 	float FireAnimDuration;
 
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Skill")
-	bool bIsWeaponSkill;
-
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Skill")
-	bool bIsPrimarySkill;
-
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Skill")
-	bool bIsSecondarySkill;
-
-	// 무기 능력 어빌리티의 선딜레이 애니메이션 시간을 지정합니다.
-	UPROPERTY(EditAnywhere, Category = "Skill")
-	float WeaponAbilityPerformDelayAnimDuration;
-
-	// 선딜레이 이후 무기 능력 어빌리티의 애니메이션 시간을 지정합니다.
-	UPROPERTY(EditAnywhere, Category = "Skill")
-	float WeaponAbilityLateAnimDuration;
-
-	// Q 어빌리티의 선딜레이 애니메이션 시간을 지정합니다.
-	UPROPERTY(EditAnywhere, Category = "Skill")
-	float PrimaryAbilityPerformDelayAnimDuration;
-
-	// 선딜레이 이후 Q 어빌리티의 애니메이션 시간을 지정합니다.
-	UPROPERTY(EditAnywhere, Category = "Skill")
-	float PrimaryAbilityLateAnimDuration;
-
-	// E 어빌리티의 선딜레이 애니메이션 시간을 지정합니다.
-	UPROPERTY(EditAnywhere, Category = "Skill")
-	float SecondaryAbilityPerformDelayAnimDuration;
-
-	// 선딜레이 이후 E 어빌리티의 애니메이션 시간을 지정합니다.
-	UPROPERTY(EditAnywhere, Category = "Skill")
-	float SecondaryAbilityLateAnimDuration;
-	
-
 	// 무기 능력 어빌리티의 애니메이션 배속을 나타냅니다.
 	UPROPERTY(BlueprintReadOnly)
 	float WeaponSkillAnimSpeed;
@@ -75,20 +41,5 @@ public:
 	virtual void NativeBeginPlay() override;
 	virtual void NativeUpdateAnimation(float DeltaSeconds) override;
 	void OnInteractingActorChanged(AActor* NewInteractingActor);
-
-private:
-	void OnWeaponAbilityPerformTimeNotified(const float& Time);
-	void OnPrimaryAbilityPerformTimeNotified(const float& Time);
-	void OnSecondaryAbilityPerformTimeNotified(const float& Time);
-
-	// 무기 능력으로 소환된 투사체가 실제로 투척이 시작되는 시간을 나타냅니다.
-	float WeaponAbilityPerformTime;
-	FTimerHandle WeaponAbilityAnimTimer;
-
-	float PrimaryAbilityPerformTime;
-	FTimerHandle PrimaryAbilityAnimTimer;
-	
-	float SecondaryAbilityPerformTime;
-	FTimerHandle SecondaryAbilityAnimTimer;
 
 };
