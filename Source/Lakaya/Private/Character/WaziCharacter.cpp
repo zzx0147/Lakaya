39fa5cc6b10f5daa5bf1f67e52fb5ea4edcaae67
yongwoo117 <ywhss117@daum.net>
2023-06-08 22:05:35 +0900

    Update Wazi, Rena ShouldAbilityStart predicate
    두 캐릭터의 스킬 사용조건을 업데이트했습니다.

--- Source/Lakaya/Private/Character/WaziCharacter.cpp
+++ Source/Lakaya/Private/Character/WaziCharacter.cpp
@@ -3,23 +3,31 @@
 
 #include "Character/WaziCharacter.h"
 
+#include "Character/BulletComponent.h"
 #include "Character/Ability/ClairvoyanceAbility.h"
 #include "Character/Ability/CoolTimedSummonAbility.h"
+#include "Character/Ability/OverdriveAbility.h"
 #include "Character/Ability/ReloadAbility.h"
 #include "Character/Ability/ResultNotifyFireAbility.h"
 
 AWaziCharacter::AWaziCharacter(const FObjectInitializer& ObjectInitializer) : Super(ObjectInitializer.
+	SetDefaultSubobjectClass(AbilityComponentNames[Primary], UOverdriveAbility::StaticClass()).
 	SetDefaultSubobjectClass(AbilityComponentNames[Secondary], UClairvoyanceAbility::StaticClass()).
 	SetDefaultSubobjectClass(AbilityComponentNames[WeaponFire], UResultNotifyFireAbility::StaticClass()).
 	SetDefaultSubobjectClass(AbilityComponentNames[WeaponAbility], UCoolTimedSummonAbility::StaticClass()).
-	SetDefaultSubobjectClass(AbilityComponentNames[WeaponReload], UReloadAbility::StaticClass()))
+	SetDefaultSubobjectClass(AbilityComponentNames[WeaponReload], UReloadAbility::StaticClass()).
+	SetDefaultSubobjectClass(ResourceComponentName, UBulletComponent::StaticClass()))
 {
+	OverdriveLateDelay = ClairvoyanceLateDelay = 0.3f;
+
+	const auto OverdriveAbility = FindAbility<UOverdriveAbility>(Primary);
 	const auto ClairvoyanceAbility = FindAbility<UClairvoyanceAbility>(Secondary);
 	const auto FireAbility = FindAbility<UResultNotifyFireAbility>(WeaponFire);
 	const auto SmokeAbility = FindAbility<UCoolTimedSummonAbility>(WeaponAbility);
 	const auto ReloadAbility = FindAbility<UReloadAbility>(WeaponReload);
 
-	//TODO: 투시 능력의 선딜레이, 후딜레이 시간동안 행동할 수 없도록 해야 합니다.
+	OverdriveAbility->OnAbilityStartTimeNotified.AddUObject(this, &AWaziCharacter::OnOverdriveStartTimeNotified);
+	ClairvoyanceAbility->OnAbilityStartTimeNotified.AddUObject(this, &AWaziCharacter::OnClairvoyanceStartTimeNotified);
 	FireAbility->OnWantsToFireChanged.AddUObject(this, &AWaziCharacter::OnWantsToFireChanged);
 	SmokeAbility->OnPerformTimeNotified.AddUObject(this, &AWaziCharacter::OnSmokePerformTimeNotified);
 	ReloadAbility->OnReloadStateChanged.AddUObject(this, &AWaziCharacter::OnReloadStateChanged);
@@ -31,14 +39,30 @@ bool AWaziCharacter::ShouldStartAbility_Implementation(EAbilityKind Kind)
 	// 네트워크 레이턴시를 고려하여 기준 시간을 현재보다 조금 더 늦게 잡아서 조금 더 이른 시점부터 능력 사용 요청을 보낼 수 있도록 합니다. 
 	const auto BasisTime = GetServerTime() + 0.1f;
 
-	// 생존하고 있고, 연막 투척 스킬 종료까지 얼마 안남았고, 재장전 종료까지 얼마 안남았고, 사격중이지 않을 때 스킬을 사용할 수 있게 합니다.
-	return GetAliveState() && SmokeEndingTime <= BasisTime && ReloadCompleteTime <= BasisTime && !bWantsToFire;
+	// 생존하고 있고, 오버드라이브 행동 종료까지 얼마 안남았고, 투시 행동 종료까지 얼마 안남았고,
+	// 연막 투척 스킬 종료까지 얼마 안남았고, 재장전 종료까지 얼마 안남았고, 사격중이지 않을 때 스킬을 사용할 수 있게 합니다.
+	return GetAliveState() && OverdriveEndingTime <= BasisTime && ClairvoyanceEndingTime <= BasisTime
+		&& SmokeEndingTime <= BasisTime && ReloadCompleteTime <= BasisTime && !bWantsToFire;
 }
 
 bool AWaziCharacter::ShouldStartAbilityOnServer_Implementation(EAbilityKind Kind)
 {
-	// 생존하고 있고, 연막 투척 스킬이 종료된 시점이고, 재장전중이지 않고 사격중이지 않을 때에 스킬을 사용할 수 있게 합니다.
-	return GetAliveState() && SmokeEndingTime <= GetServerTime() && !bIsReloading && !bWantsToFire;
+	const auto Time = GetServerTime();
+
+	// 생존하고 있고, 오버드라이브 행동이 종료되었고, 투시 행동이 종료되었고,
+	// 연막 투척 스킬이 종료된 시점이고, 재장전중이지 않고 사격중이지 않을 때에 스킬을 사용할 수 있게 합니다.
+	return GetAliveState() && OverdriveEndingTime <= Time && ClairvoyanceEndingTime <= Time
+		&& SmokeEndingTime <= Time && !bIsReloading && !bWantsToFire;
+}
+
+void AWaziCharacter::OnOverdriveStartTimeNotified(const float& Time)
+{
+	OverdriveEndingTime = Time + OverdriveLateDelay;
+}
+
+void AWaziCharacter::OnClairvoyanceStartTimeNotified(const float& Time)
+{
+	ClairvoyanceEndingTime = Time + ClairvoyanceLateDelay;
 }
 
 void AWaziCharacter::OnWantsToFireChanged(bool FireState)
