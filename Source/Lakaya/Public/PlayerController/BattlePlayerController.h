c45f995f9b523ee8bb0c0face36606e8601e86e5
yongwoo117 <ywhss117@daum.net>
2023-06-11 21:51:49 +0900

    Optimize BattlePlayerController
    불필요한 멤버함수 선언을 줄였습니다.

--- Source/Lakaya/Public/PlayerController/BattlePlayerController.h
+++ Source/Lakaya/Public/PlayerController/BattlePlayerController.h
@@ -5,6 +5,7 @@
 #include "CoreMinimal.h"
 #include "MovablePlayerController.h"
 #include "InputActionValue.h"
+#include "Character/ArmedCharacter.h"
 #include "BattlePlayerController.generated.h"
 
 UCLASS()
@@ -14,8 +15,8 @@ class LAKAYA_API ABattlePlayerController : public AMovablePlayerController
 
 public:
 	ABattlePlayerController();
-	void SetSkillWidget(class USkillWidget* NewSkillWidget) {SkillWidget = NewSkillWidget;}
-	
+	void SetSkillWidget(class USkillWidget* NewSkillWidget) { SkillWidget = NewSkillWidget; }
+
 protected:
 	virtual void SetupEnhancedInputComponent(UEnhancedInputComponent* const& EnhancedInputComponent) override;
 	virtual void SetupMappingContext(UEnhancedInputLocalPlayerSubsystem* const& InputSubsystem) override;
@@ -23,18 +24,8 @@ protected:
 	virtual void OnPossessedPawnChangedCallback(APawn* ArgOldPawn, APawn* NewPawn) override;
 
 private:
-	void PrimaryStart(const FInputActionValue& Value);
-	void PrimaryStop(const FInputActionValue& Value);
-	void SecondStart(const FInputActionValue& Value);
-	void SecondStop(const FInputActionValue& Value);
-	void FireStart(const FInputActionValue& Value);
-	void FireStop(const FInputActionValue& Value);
-	void AbilityStart(const FInputActionValue& Value);
-	void AbilityStop(const FInputActionValue& Value);
-	void ReloadStart(const FInputActionValue& Value);
-	void ReloadStop(const FInputActionValue& Value);
-	void DashStart(const FInputActionValue& Value);
-	void DashStop(const FInputActionValue& Value);
+	void StartAbility(EAbilityKind AbilityKind);
+	void StopAbility(EAbilityKind AbilityKind);
 
 	UPROPERTY(EditAnywhere, Category="Input|Weapon|Context")
 	UInputMappingContext* WeaponControlContext;
