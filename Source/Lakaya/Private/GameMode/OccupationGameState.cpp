709f973d0f3d3777284591f9123051814f4a04f2
NaHoGyeom <zzx0147@naver.com>
2023-06-12 23:28:35 +0900

    add nullptr check to addmappingcontext resultshortcutContext

--- Source/Lakaya/Private/GameMode/OccupationGameState.cpp
+++ Source/Lakaya/Private/GameMode/OccupationGameState.cpp
@@ -404,10 +404,13 @@ void AOccupationGameState::ShowGradeResultWidget(ALakayaBasePlayerState* PlayerS
 			                           &AOccupationGameState::ChangeResultWidget);
 		}
 
-//		if (const auto SubSystem = Controller->GetLocalPlayer()->GetSubsystem<UEnhancedInputLocalPlayerSubsystem>())
-//		{
-//			SubSystem->AddMappingContext(ResultShortcutContext, 200);
-//		}
+		if(Controller != nullptr && Controller->GetLocalPlayer() != nullptr)
+		{
+			if (const auto SubSystem = Controller->GetLocalPlayer()->GetSubsystem<UEnhancedInputLocalPlayerSubsystem>())
+			{
+				SubSystem->AddMappingContext(ResultShortcutContext, 200);
+			}
+		}
 
 		ShowGradeResultElementWidget(PlayerState);
 	});
