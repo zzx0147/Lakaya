9874cd216282f45b48e12efe8cd58ffb92d4cd26
NaHoGyeom <zzx0147@naver.com>
2023-06-17 23:27:34 +0900

    remove header

--- Source/Lakaya/Private/UI/RecordWidget.cpp
+++ Source/Lakaya/Private/UI/RecordWidget.cpp
@@ -2,17 +2,10 @@
 
 
 #include "UI/RecordWidget.h"
-#include "Developer/TraceAnalysis/Private/Store/AsioSocket.h"
-#include "ThirdParty/asio/1.12.2/asio/ip/tcp.hpp"
-#include "ThirdParty/asio/1.12.2/asio/io_context.hpp"
 
 void URecordWidget::NativeConstruct()
 {
 	Super::NativeConstruct();
-
-	asio::io_context mycontext;
-	
-	auto Temp = new UE::Trace::FAsioSocket(asio::ip::tcp::socket(mycontext));
 
 	
 }
