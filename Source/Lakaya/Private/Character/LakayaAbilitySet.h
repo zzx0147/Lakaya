1d5e0d1100e12842f51b9485b8763cd4a6deb924
yongwoo117 <ywhss117@daum.net>
2023-07-27 15:09:24 +0900

    Update LakayaAbilitySet.h
    주석 추가

--- Source/Lakaya/Private/Character/LakayaAbilitySet.h
+++ Source/Lakaya/Private/Character/LakayaAbilitySet.h
@@ -33,16 +33,31 @@ struct FLakayaAbilityBindInfo
 	TSubclassOf<class UGameplayAbility> Class;
 };
 
-/**  */
+/**
+ * @brief 입력을 수신하는 어빌리티들에 대한 정보를 저장하는 컨테이너 클래스입니다.
+ * 주어지는 어빌리티 시스템 컴포넌트에 능력을 등록하거나, 해제하는 기능을 구현합니다.
+ */
 UCLASS()
 class LAKAYA_API ULakayaAbilitySet : public UDataAsset
 {
 	GENERATED_BODY()
 
 public:
+	/**
+	 * @brief 어빌리티 셋에 존재하는 어빌리티들을 어빌리티 시스템에 등록합니다.
+	 * @param AbilitySystemComponent 어빌리티를 등록할 대상 어빌리티 시스템입니다.
+	 * @param InputOffset 어빌리티 입력에 대한 오프셋입니다. 기본값은 0입니다.
+	 */
 	virtual void GiveAbilities(class UAbilitySystemComponent* AbilitySystemComponent, const int32& InputOffset = 0);
+
+	/**
+	 * @brief 이 어빌리티 셋이 등록한 어빌리티들을 어빌리티 시스템에서 해제합니다.
+	 */
 	virtual void ClearAbilities();
 
+	/**
+	 * @brief 이 어빌리티 셋이 등록한 어빌리티에 대한 핸들들을 반환합니다.
+	 */
 	FORCEINLINE const TArray<struct FGameplayAbilitySpecHandle>& GetRegisteredAbilityHandles() const
 	{
 		return RegisteredAbilities;
