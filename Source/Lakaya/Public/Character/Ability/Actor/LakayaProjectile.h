35fa1dd7e74a6986c010d36329d7978bb72c984b
yongwoo117 <ywhss117@daum.net>
2023-09-03 16:17:44 +0900

    Update LakayaProjectile.h
    clean

--- Source/Lakaya/Public/Character/Ability/Actor/LakayaProjectile.h
+++ Source/Lakaya/Public/Character/Ability/Actor/LakayaProjectile.h
@@ -12,18 +12,18 @@ struct FPredictionKey;
 UENUM(BlueprintType)
 enum class EProjectileState : uint8
 {
-	// 투사체가 게임에 아무런 개입도 하지 않고 숨겨져 있는 상태입니다.
+	/** 투사체가 게임에 아무런 개입도 하지 않고 숨겨져 있는 상태입니다. */
 	Collapsed,
 
-	// 투사체가 발사되어 날아가는 중입니다.
+	/** 투사체가 발사되어 날아가는 중입니다. */
 	Perform,
 
-	// 하위 클래스에서 이 외의 다른 상태들이 필요한 경우 사용합니다.
+	/** 하위 클래스에서 이 외의 다른 상태들이 필요한 경우 사용합니다. */
 	Custom
 };
 
-DECLARE_EVENT_ThreeParams(ALakayaProjectile, FProjectileStateChanged, class ALakayaProjectile*, const EProjectileState&,
-                          const uint8&)
+DECLARE_EVENT_ThreeParams(ALakayaProjectile, FProjectileStateChanged,
+                          class ALakayaProjectile*, const EProjectileState&, const uint8&)
 
 USTRUCT(BlueprintType)
 struct FGameplayAbilityTargetData_LocationWithTime : public FGameplayAbilityTargetData
