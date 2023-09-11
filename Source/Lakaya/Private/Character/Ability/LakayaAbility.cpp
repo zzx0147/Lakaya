4c160cb4552535ed31ae9fe81e916fbda8e35f8b
yongwoo117 <ywhss117@daum.net>
2023-09-11 15:52:32 +0900

    Update ActivateAbility
    Super::ActivateAbility에서 CommitAbility을 호출하지 않도록 변경했습니다. 하위 클래스에서 수동으로 호출해야 합니다. 언리얼 5.3에서는 CommitAbility가 호출되지 않도록 변경되었으므로 이 커밋을 Revert해도 됩니다.

--- Source/Lakaya/Private/Character/Ability/LakayaAbility.cpp
+++ Source/Lakaya/Private/Character/Ability/LakayaAbility.cpp
@@ -97,7 +97,37 @@ void ULakayaAbility::ActivateAbility(const FGameplayAbilitySpecHandle Handle,
                                      const FGameplayEventData* TriggerEventData)
 {
 	Log(ActorInfo, TEXT("Activate Ability"));
-	Super::ActivateAbility(Handle, ActorInfo, ActivationInfo, TriggerEventData);
+	if (bHasBlueprintActivate)
+	{
+		// A Blueprinted ActivateAbility function must call CommitAbility somewhere in its execution chain.
+		K2_ActivateAbility();
+	}
+	else if (bHasBlueprintActivateFromEvent)
+	{
+		if (TriggerEventData)
+		{
+			// A Blueprinted ActivateAbility function must call CommitAbility somewhere in its execution chain.
+			K2_ActivateAbilityFromEvent(*TriggerEventData);
+		}
+		else
+		{
+			UE_LOG(LogAbilitySystem, Warning, TEXT("Ability %s expects event data but none is being supplied. Use Activate Ability instead of Activate Ability From Event."), *GetName());
+			bool bReplicateEndAbility = false;
+			bool bWasCancelled = true;
+			EndAbility(Handle, ActorInfo, ActivationInfo, bReplicateEndAbility, bWasCancelled);
+		}
+	}
+	else
+	{
+		// Native child classes may want to override ActivateAbility and do something like this:
+
+		// Do stuff...
+
+		// if (CommitAbility(Handle, ActorInfo, ActivationInfo))		// ..then commit the ability...
+		// 	{			
+		// 	//	Then do more stuff...
+		// 	}
+	}
 }
 
 void ULakayaAbility::CancelAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo,
