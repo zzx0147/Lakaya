46887c77e3448968b9cb701883784e0feb45de19
BinHenz <dstjer@naver.com>
2023-09-18 11:00:32 +0900

    Drone Attribute Set & Gangrim Attack Logic Modify
    드론 어트리뷰트 셋으로 체력 설정해주었고
    강림 공격이 플레이어 캐릭터 뿐만아닌 드론도 공격이 되게 수정되었습니다.

--- Source/Lakaya/Private/AI/AiDroneCharacter.cpp
+++ Source/Lakaya/Private/AI/AiDroneCharacter.cpp
@@ -3,14 +3,22 @@
 
 #include "AI/AiDroneCharacter.h"
 
+#include "AbilitySystemComponent.h"
+#include "Character/Ability/Attribute/LakayaAttributeSet.h"
+
 // Sets default values
 AAiDroneCharacter::AAiDroneCharacter()
 {
  	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
-	
+	AbilitySystem = CreateDefaultSubobject<UAbilitySystemComponent>(TEXT("AbilitySystem"));
+	AttributeSet = CreateDefaultSubobject<ULakayaAttributeSet>(TEXT("LakayaAttributeSet"));
 }
 
+UAbilitySystemComponent* AAiDroneCharacter::GetAbilitySystemComponent() const
+{
+	return AbilitySystem;
+}
 
 // Called when the game starts or when spawned
 void AAiDroneCharacter::BeginPlay()
