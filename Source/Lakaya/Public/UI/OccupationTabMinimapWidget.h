1bf4d9d50b22a396231f86d7c9b4beb03da3f922
KwonDoJun <bluetooth3333@naver.com>
2023-10-09 21:51:58 +0900

    MinimapWidget Refactoring.
    MinimapWidget Refactoring.

--- Source/Lakaya/Public/UI/OccupationTabMinimapWidget.h
+++ Source/Lakaya/Public/UI/OccupationTabMinimapWidget.h
@@ -16,28 +16,14 @@ class LAKAYA_API UOccupationTabMinimapWidget : public UTabMinimapWidget
 {
 	GENERATED_BODY()
 
+public:
+	virtual UImage* CreatePlayerImage(const ETeam& NewTeam, const bool bMyPlayer = false) override;
+	
 protected:
 	virtual void NativeConstruct() override;
 	virtual void NativeTick(const FGeometry& MyGeometry, float InDeltaTime) override;
 
 	virtual FVector2d ConvertWorldToMiniMapCoordinates(const FVector2D& PlayerLocation, const FVector2D& MiniMapSize) override;
-
 	virtual void UpdatePlayerPosition(const ETeam& Team) override;
 	virtual void UpdatePlayerPosition(const ETeam& NewTeam, const TWeakObjectPtr<ALakayaBasePlayerState> NewPlayerState) override;
-	
-public:
-	virtual UImage* CreatePlayerImage(const ETeam& NewTeam, const bool bMyPlayer = false) override;
-
-private:
-	// 미니맵상에 Anti팀을 표시하는 아이콘 텍스처입니다.
-	UPROPERTY(EditAnywhere)
-	TObjectPtr<UTexture2D> AntiIcon;
-
-	// 미니맵상에 Pro팀을 표시하는 아이콘 텍스처입니다.
-	UPROPERTY(EditAnywhere)
-	TObjectPtr<UTexture2D> ProIcon;
-
-	// AntiIcon, ProIcon을 담는 맵입니다.
-	UPROPERTY()
-	TMap<ETeam, TObjectPtr<UTexture2D>> TeamIcons;
 };
