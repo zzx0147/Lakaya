b710fc3ac3717f8dfeea53602c693f0215de6b4f
KwonDoJun <bluetooth3333@naver.com>
2023-10-14 18:30:22 +0900

    CapturePorgressBar.
    점령 진행률과, 점령 표시 UI와 연동했습니다.

--- Source/Lakaya/Private/GameMode/OccupationGameState.cpp
+++ Source/Lakaya/Private/GameMode/OccupationGameState.cpp
@@ -638,6 +638,32 @@ void AOccupationGameState::UpdateOccupyExpressWidget(const ETeam& Team, const ui
 	}
 }
 
+void AOccupationGameState::UpdateExpressWidget(const ETeam& Team, const uint8& Id, const float& Progress)
+{
+	UProgressBar** Bar = OccupyBarMaps.Find(Id);
+	FSlateBrush ChargeImageBrush;
+	if (Bar != nullptr && *Bar != nullptr)
+	{
+		if (Team == ETeam::Anti)
+		{
+			ChargeImageBrush.SetResourceObject(OccupyExpressWidget->GetAntiChargeImage());
+		}
+		else if (Team == ETeam::Pro)
+		{
+			ChargeImageBrush.SetResourceObject(OccupyExpressWidget->GetProChargeImage());
+		}
+	
+		(*Bar)->WidgetStyle.SetFillImage(ChargeImageBrush);
+		(*Bar)->WidgetStyle.FillImage = ChargeImageBrush;
+	
+		(*Bar)->SetPercent(Progress / 4);
+	}
+	else
+	{
+		UE_LOG(LogTemp, Warning, TEXT("Bar && *Bar is null."));
+	}
+}
+
 void AOccupationGameState::OnEnemySpotted(const ETeam& EnemyTeam, ALakayaBasePlayerState* Enemy)
 {
 	// 같은 팀의 정보는 필요 없으므로, 리턴합니다.
