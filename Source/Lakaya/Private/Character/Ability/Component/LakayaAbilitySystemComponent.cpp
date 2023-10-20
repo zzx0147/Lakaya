f7bb705babec91f95a399614ac972c372569f5b4
yongwoo117 <ywhss117@daum.net>
2023-10-20 22:53:23 +0900

    Update LakayaAbilitySystemComponent
    어빌리티 시스템 컴포넌트에서 입력을 가로채는 기능을 구현하기 위한 베이스 코드를 추가했습니다.

--- Source/Lakaya/Private/Character/Ability/Component/LakayaAbilitySystemComponent.cpp
+++ Source/Lakaya/Private/Character/Ability/Component/LakayaAbilitySystemComponent.cpp
@@ -3,6 +3,39 @@
 #include "AbilitySystemGlobals.h"
 #include "GameplayTagResponseTable.h"
 
+void ULakayaAbilitySystemComponent::AddInputInhibitedDelegate(const int32& InputID,
+                                                              const FInputInhibitedDelegate& PressedDelegate,
+                                                              const FInputInhibitedDelegate& ReleasedDelegate)
+{
+	const auto Pair = InputInhibitedDelegates.Find(InputID);
+	if (Pair && Pair->IsBound())
+	{
+		UE_LOG(LogTemp, Warning, TEXT("InputID %d is already bound."), InputID);
+	}
+	InputInhibitedDelegates.Emplace(InputID, {PressedDelegate, ReleasedDelegate});
+}
+
+void ULakayaAbilitySystemComponent::RemoveInputInhibitedDelegate(const int32& InputID)
+{
+	InputInhibitedDelegates.Remove(InputID);
+}
+
+void ULakayaAbilitySystemComponent::AbilityLocalInputPressed(int32 InputID)
+{
+	if (!InputInhibitedDelegates.FindOrAdd(InputID).PressedIfBound(InputID))
+	{
+		Super::AbilityLocalInputPressed(InputID);
+	}
+}
+
+void ULakayaAbilitySystemComponent::AbilityLocalInputReleased(int32 InputID)
+{
+	if (!InputInhibitedDelegates.FindOrAdd(InputID).ReleasedIfBound(InputID))
+	{
+		Super::AbilityLocalInputReleased(InputID);
+	}
+}
+
 void ULakayaAbilitySystemComponent::InitAbilityActorInfo(AActor* InOwnerActor, AActor* InAvatarActor)
 {
 	//TODO: 언리얼 5.3의 스크립트이므로, 5.3에서는 제거해야 합니다.
--- Source/Lakaya/Public/Character/Ability/Component/LakayaAbilitySystemComponent.h
+++ Source/Lakaya/Public/Character/Ability/Component/LakayaAbilitySystemComponent.h
@@ -4,12 +4,45 @@
 #include "AbilitySystemComponent.h"
 #include "LakayaAbilitySystemComponent.generated.h"
 
+DECLARE_DELEGATE_OneParam(FInputInhibitedDelegate, const int32& /* InhibitedInputID */)
 
 UCLASS(ClassGroup=(Custom), meta=(BlueprintSpawnableComponent))
 class LAKAYA_API ULakayaAbilitySystemComponent : public UAbilitySystemComponent
 {
 	GENERATED_BODY()
 
+	struct FInputInhibitedDelegatePair
+	{
+		FInputInhibitedDelegate PressedDelegate, ReleasedDelegate;
+
+		FORCEINLINE bool IsBound() const { return PressedDelegate.IsBound() || ReleasedDelegate.IsBound(); }
+
+		FORCEINLINE bool PressedIfBound(const int32& InputID)
+		{
+			return PressedDelegate.ExecuteIfBound(InputID) || ReleasedDelegate.IsBound();
+		}
+
+		FORCEINLINE bool ReleasedIfBound(const int32& InputID)
+		{
+			return ReleasedDelegate.ExecuteIfBound(InputID) || PressedDelegate.IsBound();
+		}
+	};
+
+public:
+	/** 이 InputID에 대한 입력을 가로채도록 합니다. */
+	void AddInputInhibitedDelegate(const int32& InputID, const FInputInhibitedDelegate& PressedDelegate,
+	                               const FInputInhibitedDelegate& ReleasedDelegate = {});
+
+	/** 이 InputID가 방해받지 않도록 합니다. */
+	void RemoveInputInhibitedDelegate(const int32& InputID);
+
+	virtual void AbilityLocalInputPressed(int32 InputID) override;
+	virtual void AbilityLocalInputReleased(int32 InputID) override;
+
 protected:
 	virtual void InitAbilityActorInfo(AActor* InOwnerActor, AActor* InAvatarActor) override;
+
+private:
+	/** 입력을 가로채는 델리게이트 맵입니다. */
+	TMap<int32, FInputInhibitedDelegatePair> InputInhibitedDelegates;
 };
