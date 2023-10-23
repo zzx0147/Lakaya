1fc93c49a00f67f673a464f4a98a25b8e5e8c696
NaHoGyeom <zzx0147@naver.com>
2023-10-23 20:54:54 +0900

    build setting and disable decal rotating function

--- Source/Lakaya/Private/GameplayCueNotify/LakayaGameplayCueNotify_Burst.cpp
+++ Source/Lakaya/Private/GameplayCueNotify/LakayaGameplayCueNotify_Burst.cpp
@@ -5,31 +5,31 @@
 #include "GameplayCueNotifyTypes.h"
 
 
-bool ULakayaGameplayCueNotify_Burst::OnExecute_Implementation(AActor* Target,
-                                                              const FGameplayCueParameters& Parameters) const
+bool ULakayaGameplayCueNotify_Burst::OnExecute_Implementation(AActor* Target, const FGameplayCueParameters& Parameters) const
 {
-	UWorld* World = (Target ? Target->GetWorld() : GetWorld());
-	
-	FGameplayCueNotify_SpawnContext SpawnContext(World, Target, Parameters);
-	
-	if(DefaultPlacementInfo.bOverrideRotation && bUseDefaultPlacementInfoToAdditiveRotation)
-	{
-		OverridePlacementInfo = DefaultPlacementInfo;
-		
-		OverridePlacementInfo.RotationOverride = SpawnContext.HitResult->ImpactNormal.Rotation().Add(DefaultPlacementInfo.RotationOverride.Pitch,DefaultPlacementInfo.RotationOverride.Yaw,DefaultPlacementInfo.RotationOverride.Roll);
-	}
-	SpawnContext.SetDefaultSpawnCondition(&DefaultSpawnCondition);
-	SpawnContext.SetDefaultPlacementInfo(&OverridePlacementInfo);
-	
-	
-	if (DefaultSpawnCondition.ShouldSpawn(SpawnContext))
-	{
-		FGameplayCueNotify_SpawnResult SpawnResult;
-	
-		BurstEffects.ExecuteEffects(SpawnContext, SpawnResult);
-		
-		OnBurst(Target, Parameters, SpawnResult);
-	}
+	Super::OnExecute_Implementation(Target, Parameters);
+	// UWorld* World = (Target ? Target->GetWorld() : GetWorld());
+	//
+	// FGameplayCueNotify_SpawnContext SpawnContext(World, Target, Parameters);
+	//
+	// if(DefaultPlacementInfo.bOverrideRotation && bUseDefaultPlacementInfoToAdditiveRotation)
+	// {
+	// 	OverridePlacementInfo = DefaultPlacementInfo;
+	// 	
+	// 	OverridePlacementInfo.RotationOverride = SpawnContext.HitResult->ImpactNormal.Rotation().Add(DefaultPlacementInfo.RotationOverride.Pitch,DefaultPlacementInfo.RotationOverride.Yaw,DefaultPlacementInfo.RotationOverride.Roll);
+	// }
+	// SpawnContext.SetDefaultSpawnCondition(&DefaultSpawnCondition);
+	// SpawnContext.SetDefaultPlacementInfo(&OverridePlacementInfo);
+	//
+	//
+	// if (DefaultSpawnCondition.ShouldSpawn(SpawnContext))
+	// {
+	// 	FGameplayCueNotify_SpawnResult SpawnResult;
+	//
+	// 	BurstEffects.ExecuteEffects(SpawnContext, SpawnResult);
+	// 	
+	// 	OnBurst(Target, Parameters, SpawnResult);
+	// }
 
 	return false;
 }
