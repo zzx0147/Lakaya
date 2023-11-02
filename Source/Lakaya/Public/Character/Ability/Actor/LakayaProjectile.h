509c2a0d0175e718dfbd8a042779197fea0a2d45
yongwoo117 <ywhss117@daum.net>
2023-11-02 11:23:23 +0900

    Update LakayaProjectile.h
    ProjectileLaunchVelocity에 접근할 수 있도록 변경했습니다.

--- Source/Lakaya/Public/Character/Ability/Actor/LakayaProjectile.h
+++ Source/Lakaya/Public/Character/Ability/Actor/LakayaProjectile.h
@@ -188,6 +188,9 @@ public:
 	UFUNCTION(BlueprintGetter)
 	UProjectileMovementComponent* GetProjectileMovementComponent() const { return ProjectileMovementComponent; }
 
+	UFUNCTION(BlueprintGetter)
+	const float& GetProjectileLaunchVelocity() const { return ProjectileLaunchVelocity; }
+
 	/**
 	 * 투사체의 상태가 변경되면 호출되는 이벤트입니다. 로컬에서 예측적으로 투사체의 상태를 변경할 때에도 호출됩니다.
 	 * 이 이벤트에서 투사체의 상태를 변경할만한 함수를 호출하면 예기치 못한 버그가 발생할 수 있습니다. (예: ThrowProjectile)
@@ -368,7 +371,7 @@ private:
 	UPROPERTY(Replicated, Transient)
 	FProjectileThrowData ThrowData;
 
-	UPROPERTY(EditAnywhere)
+	UPROPERTY(EditAnywhere, BlueprintGetter=GetProjectileLaunchVelocity)
 	float ProjectileLaunchVelocity;
 
 	FProjectileState LocalState;
