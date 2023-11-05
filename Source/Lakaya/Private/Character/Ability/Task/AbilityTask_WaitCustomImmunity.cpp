d5faa1d89a3a38798e8a4d47e8421e87273d15cc
yongwoo117 <ywhss117@daum.net>
2023-11-05 19:14:38 +0900

    Add WaitCustomImmunity task
    커스텀 면역을 부여하고, 면역조건에 대한 이벤트에 바인딩하는 새로운 태스크를 추가했습니다.

---
+++ Source/Lakaya/Private/Character/Ability/Task/AbilityTask_WaitCustomImmunity.cpp
@@ -0,0 +1,66 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Character/Ability/Task/AbilityTask_WaitCustomImmunity.h"
+
+#include "Character/Ability/Component/LakayaAbilitySystemComponent.h"
+#include "Character/Ability/CustomImmunity/LakayaAbilityCustomImmunity.h"
+
+UAbilityTask_WaitCustomImmunity* UAbilityTask_WaitCustomImmunity::WaitCustomImmunity(UGameplayAbility* OwningAbility,
+                                                                                     ULakayaAbilityCustomImmunity* CustomImmunity, bool bTriggerOnce, bool bTriggerWhenSameClass)
+{
+	const auto MyObj = NewAbilityTask<ThisClass>(OwningAbility);
+	MyObj->CustomImmunity = CustomImmunity;
+	MyObj->bIsTriggerOnce = bTriggerOnce;
+	MyObj->bOnlySameClass = bTriggerWhenSameClass;
+	return MyObj;
+}
+
+void UAbilityTask_WaitCustomImmunity::Activate()
+{
+	Super::Activate();
+
+	const auto ASC = CastChecked<ULakayaAbilitySystemComponent>(AbilitySystemComponent);
+	if (CustomImmunity.IsValid())
+	{
+		ASC->GiveCustomImmunity(CustomImmunity.Get());
+		OnImmunedByCustomImmunityHandle = ASC->OnImmunedByCustomImmunity.AddUObject(
+			this, &UAbilityTask_WaitCustomImmunity::OnImmunedByCustomImmunity);
+	}
+	else
+	{
+		if (ShouldBroadcastAbilityTaskDelegates())
+		{
+			OnCanceled.Broadcast();
+		}
+		EndTask();
+	}
+}
+
+void UAbilityTask_WaitCustomImmunity::OnDestroy(bool bInOwnerFinished)
+{
+	if (const auto ASC = Cast<ULakayaAbilitySystemComponent>(AbilitySystemComponent))
+	{
+		ASC->RemoveCustomImmunity(CustomImmunity.Get());
+		ASC->OnImmunedByCustomImmunity.Remove(OnImmunedByCustomImmunityHandle);
+	}
+	Super::OnDestroy(bInOwnerFinished);
+}
+
+void UAbilityTask_WaitCustomImmunity::OnImmunedByCustomImmunity(const FGameplayEffectSpec& Spec,
+                                                                const ULakayaAbilityCustomImmunity* InCustomImmunity)
+{
+	if (bOnlySameClass && InCustomImmunity != CustomImmunity.Get())
+	{
+		return;
+	}
+
+	if (ShouldBroadcastAbilityTaskDelegates())
+	{
+		OnImmune.Broadcast(Spec, InCustomImmunity);
+	}
+	if (bIsTriggerOnce)
+	{
+		EndTask();
+	}
+}
