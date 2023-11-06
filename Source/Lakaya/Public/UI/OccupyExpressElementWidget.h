023bb8099e909aca1133d43c865e9d3da037bb19
KwonDoJun <bluetooth3333@naver.com>
2023-11-06 19:09:37 +0900

    class create.
    class create.

---
+++ Source/Lakaya/Public/UI/OccupyExpressElementWidget.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "OccupyExpressElementWidget.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class LAKAYA_API UOccupyExpressElementWidget : public UUserWidget
+{
+	GENERATED_BODY()
+	
+};
