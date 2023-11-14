ee1c7248cc6143afef43114450f83e25960bcc0f
BinHenz <dstjer@naver.com>
2023-11-14 12:51:07 +0900

    AI Individual LiveScoreBoard Widget Modify

--- Source/Lakaya/Private/UI/IndividualWidget/IndividualLiveScoreElement.cpp
+++ Source/Lakaya/Private/UI/IndividualWidget/IndividualLiveScoreElement.cpp
@@ -3,21 +3,85 @@
 
 #include "UI/IndividualWidget/IndividualLiveScoreElement.h"
 
+#include "Components/Image.h"
 #include "Components/TextBlock.h"
-#include "GameFramework/PlayerState.h"
-
-void UIndividualLiveScoreElement::SetLiveScore(const FString& PlayerName, int32 PlayerRank)
-{
-	PlayerRank_Text->SetText(FText::AsNumber(PlayerRank));
-	PlayerRankName_Text->SetText(FText::FromString(PlayerName));
-}
 
 void UIndividualLiveScoreElement::NativeConstruct()
 {
 	Super::NativeConstruct();
 
-	// InitializeWidgetPtr(RankBoxImage, TEXT("RankBox_Etc_Img"));
-	// InitializeWidgetPtr(PlayerRankTextBlock, TEXT("PlayerRank_Text"));
-	// InitializeWidgetPtr(PlayerRankNameTextBlock, TEXT("PlayerRankName_Text"));
+#pragma region InitAndNullCheck
+	RankBox_Img = Cast<UImage>(GetWidgetFromName("RankBox_Img"));
+	PlayerCheck_Img = Cast<UImage>(GetWidgetFromName("PlayerCheck_Img"));
+	NameText = Cast<UTextBlock>(GetWidgetFromName(TEXT("NameText")));
+	RankText = Cast<UTextBlock>(GetWidgetFromName(TEXT("RankText")));
+	KillText = Cast<UTextBlock>(GetWidgetFromName(TEXT("KillText")));
 
+	check(NameText != nullptr);
+	check(RankText != nullptr);
+	check(KillText != nullptr);
+	
+	check(RankBox_Img != nullptr);
+	check(PlayerCheck_Img != nullptr);
+#pragma endregion
+}
+
+void UIndividualLiveScoreElement::Set1stElementBoxImage()
+{
+	if (RankBox_Img.IsValid())
+	{
+		FString ImagePath = TEXT("/Game/UI_2/UI_IngameIndividual/T_InGame_Individual_Rank_Box"); // 이미지 경로
+		UTexture2D* LoadedTexture = Cast<UTexture2D>(StaticLoadObject(UTexture2D::StaticClass(), nullptr, *ImagePath));
+    
+		if (LoadedTexture)
+		{
+			RankBox_Img->SetBrushFromTexture(LoadedTexture);
+		}
+	}
+}
+
+void UIndividualLiveScoreElement::SetOtherElementBoxImage()
+{
+	if (RankBox_Img.IsValid())
+	{
+		FString ImagePath = TEXT("/Game/UI_2/UI_IngameIndividual/T_InGame_Individual_Rank_Box_2"); // 이미지 경로
+		UTexture2D* LoadedTexture = Cast<UTexture2D>(StaticLoadObject(UTexture2D::StaticClass(), nullptr, *ImagePath));
+    
+		if (LoadedTexture)
+		{
+			RankBox_Img->SetBrushFromTexture(LoadedTexture);
+		}
+	}
+}
+
+void UIndividualLiveScoreElement::SetPlayerCheckVisible()
+{
+	PlayerCheck_Img->SetVisibility(ESlateVisibility::Visible);
+}
+
+void UIndividualLiveScoreElement::SetPlayerCheckHidden()
+{
+	PlayerCheck_Img->SetVisibility(ESlateVisibility::Hidden);
+}
+
+void UIndividualLiveScoreElement::SetIndividualRank(int32 RankCount)
+{
+	if (RankText.IsValid())	RankText->SetText(FText::AsNumber(RankCount));
+}
+
+void UIndividualLiveScoreElement::SetPlayerName(const FText& PlayerName) const
+{
+	if (NameText.IsValid()) NameText->SetText(PlayerName);
+}
+
+void UIndividualLiveScoreElement::SetPlayerName(const FString& PlayerName) const
+{
+	SetPlayerName(FText::FromString(PlayerName));
+}
+
+void UIndividualLiveScoreElement::SetKillCount(const uint16& KillCount) const
+{
+	IndividualKillCount = KillCount;
+	
+	if (KillText.IsValid())	KillText->SetText(FText::AsNumber(KillCount));
 }
--- Source/Lakaya/Public/UI/IndividualWidget/IndividualLiveScoreElement.h
+++ Source/Lakaya/Public/UI/IndividualWidget/IndividualLiveScoreElement.h
@@ -3,7 +3,7 @@
 #pragma once
 
 #include "CoreMinimal.h"
-#include "UI/IndividualWidget/IndividualBaseWidget.h"
+#include "UI/ScoreBoardElement.h"
 #include "IndividualLiveScoreElement.generated.h"
 
 /**
@@ -11,27 +11,43 @@
  */
 class UImage;
 class UTextBlock;
-class APlayerState;
 
 UCLASS()
-class LAKAYA_API UIndividualLiveScoreElement : public UIndividualBaseWidget
+class LAKAYA_API UIndividualLiveScoreElement : public UScoreBoardElement
 {
 	GENERATED_BODY()
-
-public:
-	void SetLiveScore(const FString& PlayerName, int32 PlayerRank);
 
 protected:
 	virtual void NativeConstruct() override;
 
+public:
+	// 1등의 Element 박스 이미지를 설정합니다.
+	void Set1stElementBoxImage();
+
+	// 2~6등의 Element 박스 이미지를 설정합니다.
+	void SetOtherElementBoxImage();
+
+	void SetPlayerCheckVisible();
+	void SetPlayerCheckHidden();
+	
+	// 개인전 등수를 설정합니다.
+	void SetIndividualRank(int32 RankCount);
+	
+	// 플레이어 이름을 설정합니다.
+	virtual void SetPlayerName(const FText& PlayerName) const override;
+	virtual void SetPlayerName(const FString& PlayerName) const override;
+
+	// 누적 킬 횟수를 업데이트합니다.
+	virtual void SetKillCount(const uint16& KillCount) const override;
+	const uint16& GetKillCount() const { return IndividualKillCount; }
+
+	mutable uint16 IndividualKillCount;
+
+	TWeakObjectPtr<UImage> RankBox_Img;
+	TWeakObjectPtr<UImage> PlayerCheck_Img;
 private:
-	UPROPERTY(meta = (BindWidget))
-	UImage* RankBox_Img;
-	
-	UPROPERTY(meta = (BindWidget))
-	UTextBlock* PlayerRank_Text;
-	
-	UPROPERTY(meta = (BindWidget))
-	UTextBlock*  PlayerRankName_Text;
+	TWeakObjectPtr<UTextBlock> RankText;
+	TWeakObjectPtr<UTextBlock>  KillText;
+	TWeakObjectPtr<UTextBlock>  NameText;
 	
 };
