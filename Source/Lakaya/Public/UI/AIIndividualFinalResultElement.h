f1c499cb891082c11bb3bac4fd890dcfb9ad4d55
BinHenz <dstjer@naver.com>
2023-11-15 14:11:42 +0900

    AI Individual FinalResultElement GradeImage Add
    AI 개인전 최종 결과창에서 등수를 텍스트로 표현하던걸 이미지로 변경했습니다.
    텍스트로 표현 할때로 롤백 바로 가능하게 구현했습니다.

--- Source/Lakaya/Public/UI/AIIndividualFinalResultElement.h
+++ Source/Lakaya/Public/UI/AIIndividualFinalResultElement.h
@@ -14,36 +14,42 @@ class LAKAYA_API UAIIndividualFinalResultElement : public UUserWidget
 {
 	GENERATED_BODY()
 	
-	public:
-    	explicit UAIIndividualFinalResultElement(const FObjectInitializer& ObjectInitializer);
-    	void SetGrade(const uint32& NewGrade);
-    	void SetPlayer(TWeakObjectPtr<class ALakayaBasePlayerState> NewPlayer);
+public:
+	explicit UAIIndividualFinalResultElement(const FObjectInitializer& ObjectInitializer);
+	void SetGrade(const uint32& NewGrade);
+	void SetPlayer(TWeakObjectPtr<class ALakayaBasePlayerState> NewPlayer);
     
-    protected:
-    	virtual void NativeConstruct() override;
+protected:
+	virtual void NativeConstruct() override;
     
-    private:
-    	UPROPERTY(meta = (BindWidget))
-    	TObjectPtr<class UImage> Character3DImage;
+private:
+	UPROPERTY(meta = (BindWidget))
+	TObjectPtr<class UImage> Character3DImage;
     
-    	UPROPERTY(meta = (BindWidget))
-    	TObjectPtr<UImage> CursorImage;
+	UPROPERTY(meta = (BindWidget))
+	TObjectPtr<UImage> CursorImage;
     
-    	UPROPERTY(meta = (BindWidget))
-    	TObjectPtr<UImage> UserInfoBoxImage;
+	UPROPERTY(meta = (BindWidget))
+	TObjectPtr<UImage> UserInfoBoxImage;
     
-    	UPROPERTY(meta = (BindWidget))
-    	TObjectPtr<UImage> UserInfoBoxOutlineImage;
+	UPROPERTY(meta = (BindWidget))
+	TObjectPtr<UImage> UserInfoBoxOutlineImage;
+
+	UPROPERTY(meta = (BindWidget))
+	TObjectPtr<UImage> GradeImage;
+	
+	UPROPERTY(meta = (BindWidget))
+	TObjectPtr<class UTextBlock> GradeText;
+	
+	UPROPERTY(meta = (BindWidget))
+	TObjectPtr<UTextBlock> KillScoreText;
     
-    	UPROPERTY(meta = (BindWidget))
-    	TObjectPtr<class UTextBlock> GradeText;
+	UPROPERTY(meta = (BindWidget))
+	TObjectPtr<UTextBlock> UserIdText;
     
-    	UPROPERTY(meta = (BindWidget))
-    	TObjectPtr<UTextBlock> KillScoreText;
-    
-    	UPROPERTY(meta = (BindWidget))
-    	TObjectPtr<UTextBlock> UserIdText;
-    
-    	UPROPERTY(EditDefaultsOnly)
-    	TMap<FName, UTexture2D*> Character3DImageMaterialMap;
+	UPROPERTY(EditDefaultsOnly)
+	TMap<FName, UTexture2D*> Character3DImageMaterialMap;
+
+	UPROPERTY(EditDefaultsOnly)
+	TMap<FName, UTexture2D*> GradeImageMap;
 };
