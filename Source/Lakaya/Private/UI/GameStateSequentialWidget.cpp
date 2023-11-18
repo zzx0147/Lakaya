a6320cd0495866f949b0636a61100ddb0c602bb4
yongwoo117 <ywhss117@daum.net>
2023-11-18 10:41:52 +0900

    Update GameStateSequentialWidget

--- Source/Lakaya/Private/UI/GameStateSequentialWidget.cpp
+++ Source/Lakaya/Private/UI/GameStateSequentialWidget.cpp
@@ -2,3 +2,19 @@
 
 
 #include "UI/GameStateSequentialWidget.h"
+
+#include "CommonActivatableWidget.h"
+#include "CommonActivatableWidgetSwitcher.h"
+
+void UGameStateSequentialWidget::SwitchTo(UCommonActivatableWidget* Widget)
+{
+	if (ensure(IsValid(WidgetSwitcher)) && IsValid(Widget))
+	{
+		if (!WidgetSwitcher->HasChild(Widget))
+		{
+			WidgetSwitcher->AddChild(Widget);
+		}
+
+		WidgetSwitcher->SetActiveWidget(Widget);
+	}
+}
--- Source/Lakaya/Public/UI/GameStateSequentialWidget.h
+++ Source/Lakaya/Public/UI/GameStateSequentialWidget.h
@@ -6,7 +6,12 @@
 #include "Blueprint/UserWidget.h"
 #include "GameStateSequentialWidget.generated.h"
 
+class UCommonActivatableWidget;
+class UIntroWidget;
+class UGameLobbyCharacterSelectWidget;
+class ULoadingWidget;
 class UCommonActivatableWidgetSwitcher;
+
 /**
  * 
  */
@@ -16,6 +21,38 @@ class LAKAYA_API UGameStateSequentialWidget : public UUserWidget
 	GENERATED_BODY()
 
 public:
-	UPROPERTY(meta=(BindWidget))
-	UCommonActivatableWidgetSwitcher* WidgetSwitcher;
+	UFUNCTION(BlueprintGetter)
+	UCommonActivatableWidgetSwitcher* GetWidgetSwitcher() const { return WidgetSwitcher; }
+
+	UFUNCTION(BlueprintGetter)
+	ULoadingWidget* GetLoadingWidget() const { return LoadingWidget; }
+
+	UFUNCTION(BlueprintGetter)
+	UGameLobbyCharacterSelectWidget* GetCharacterSelectWidget() const { return CharacterSelectWidget; }
+
+	UFUNCTION(BlueprintGetter)
+	UIntroWidget* GetGameIntroWidget() const { return GameIntroWidget; }
+
+	UFUNCTION(BlueprintGetter)
+	UCommonActivatableWidget* GetInGameOverlayWidget() const { return InGameOverlayWidget; }
+
+	/** 해당 위젯에 스위처에 보여지도록 합니다. 자식이 아니던 위젯은 자동으로 추가해서 보여주는데, 비활성화될 때 다시 제거되지는 않습니다. */
+	UFUNCTION(BlueprintCallable)
+	void SwitchTo(UCommonActivatableWidget* Widget);
+
+private:
+	UPROPERTY(BlueprintGetter="GetWidgetSwitcher", meta=(BindWidget))
+	TObjectPtr<UCommonActivatableWidgetSwitcher> WidgetSwitcher;
+
+	UPROPERTY(BlueprintGetter="GetLoadingWidget", meta=(BindWidgetOptional))
+	TObjectPtr<ULoadingWidget> LoadingWidget;
+
+	UPROPERTY(BlueprintGetter="GetCharacterSelectWidget", meta=(BindWidgetOptional))
+	TObjectPtr<UGameLobbyCharacterSelectWidget> CharacterSelectWidget;
+
+	UPROPERTY(BlueprintGetter="GetGameIntroWidget", meta=(BindWidgetOptional))
+	TObjectPtr<UIntroWidget> GameIntroWidget;
+
+	UPROPERTY(BlueprintGetter="GetInGameOverlayWidget", meta=(BindWidgetOptional))
+	TObjectPtr<UCommonActivatableWidget> InGameOverlayWidget;
 };
